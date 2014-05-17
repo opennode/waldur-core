9b752d381eacbe972c02b1a0bd957870d45060f8
Ilja Livenson <ilja.livenson@gmail.com>
2014-05-17 15:55:59 +0400

    Added an index stub with pricing list (NC-12)

--- nodeconductor/base/urls.py
+++ nodeconductor/base/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from nodeconductor.base.views import IndexView
 
+
 urlpatterns = patterns(
     '',
-    url(r'^$', IndexView.as_view(), name='base-index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 )
