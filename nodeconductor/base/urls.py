6eee252c7820656e21ef13ad237b88aa204a9bea
Ilja Livenson <ilja.livenson@gmail.com>
2014-05-17 23:12:49 +0400

    Multiple changes to layout, initial login/logout implementation (NC-12)

--- nodeconductor/base/urls.py
+++ nodeconductor/base/urls.py
@@ -8,4 +8,7 @@ from nodeconductor.base.views import IndexView
 urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+
 )
