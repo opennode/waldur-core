2ebb02448fb68e5e9355b9d3c679a7b7fa9a3a49
Johannes Meixner <xmj@chaot.net>
2014-07-01 17:57:49 +0300

    Account app deemed unnecessary. (NC-13)

--- nodeconductor/account/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class NodeUser(User):
-    """
-    Preliminary User class.
-    As always, requires Username, password and email to be present.
-    """
-    pass
