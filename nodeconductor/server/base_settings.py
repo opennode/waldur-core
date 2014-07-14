670ff81aa9be39366c29eb23e1b31b587fdd5db8
Ilja Livenson <ilja.livenson@gmail.com>
2014-07-14 13:16:51 +0300

    Re-enabled core application

--- nodeconductor/server/base_settings.py
+++ nodeconductor/server/base_settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'nodeconductor.core',
     'nodeconductor.structure',
     'nodeconductor.cloud',
     'nodeconductor.iaas',
