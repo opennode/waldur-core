64a422e0fc35d13ac86bed8314288f6e39eef197
Ihor Kaharlichenko <ihor@opennodecloud.com>
2014-07-24 19:00:08 +0300

    Project based flavor read ACLs

--- nodeconductor/cloud/views.py
+++ nodeconductor/cloud/views.py
@@ -1,13 +1,17 @@
+from rest_framework import filters
 from rest_framework import viewsets
 
 from nodeconductor.cloud import models
 from nodeconductor.cloud import serializers
+from nodeconductor.core import permissions
 
 
 class FlavorViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Flavor.objects.all()
     serializer_class = serializers.FlavorSerializer
     lookup_field = 'uuid'
+    filter_backends = (filters.DjangoObjectPermissionsFilter,)
+    permission_classes = (permissions.DjangoObjectLevelPermissions,)
 
 
 class OpenStackCloudViewSet(viewsets.ModelViewSet):
