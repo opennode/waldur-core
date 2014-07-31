eea12a0a430ef3300d44686e1053d37bc43f2a82
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2014-07-31 19:55:34 +0300

    Removed tags
    nc-62
    Used material:
    http://stackoverflow.com/questions/9879687/adding-a-manytomanywidget-to-the-reverse-of-a-manytomanyfield-in-the-django-admi
    https://snipt.net/chrisdpratt/symmetrical-manytomany-filter-horizontal-in-django-admin/

--- nodeconductor/structure/admin.py
+++ nodeconductor/structure/admin.py
@@ -4,20 +4,23 @@ from django import forms
 from nodeconductor.structure import models
 from nodeconductor.cloud.models import Cloud
 
+
 class CloudAdminForm(forms.ModelForm):
-    clouds = forms.ModelMultipleChoiceField(Cloud.objects.all())
+    clouds = forms.ModelMultipleChoiceField(Cloud.objects.all(),
+                                            required=False)
 
     def __init__(self, *args, **kwargs):
         super(CloudAdminForm, self).__init__(*args, **kwargs)
         if self.instance.pk:
-            self.initial['clouds'] = self.instance.tags.values_list('pk', flat=True)
+            self.initial['clouds'] = self.instance.clouds.values_list('pk', flat=True)
 
     def save(self, *args, **kwargs):
         instance = super(CloudAdminForm, self).save(*args, **kwargs)
         if instance.pk:
-            instance.tags.clear()
-            instance.tags.add(*self.cleaned_data['tags'])
+            instance.clouds.clear()
+            instance.clouds.add(*self.cleaned_data['clouds'])
         return instance
+
 
 class ProjectAdmin(admin.ModelAdmin):
     form = CloudAdminForm
