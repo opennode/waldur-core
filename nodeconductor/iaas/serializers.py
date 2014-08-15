20000af2899897d25058f82855793d977d2bf3eb
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2014-08-15 14:23:17 +0300

    Add user specific fields filtering

--- nodeconductor/iaas/serializers.py
+++ nodeconductor/iaas/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from nodeconductor.structure.models import Role
+
 from nodeconductor.iaas import models
 from nodeconductor.core import models as core_models
 from nodeconductor.core.serializers import PermissionFieldFilteringMixin
@@ -28,9 +30,22 @@ class InstanceSerializer(PermissionFieldFilteringMixin,
 
     class Meta(object):
         model = models.Instance
-        fields = ('url', 'hostname', 'template', 'cloud', 'project', 'flavor', 'state')
+        fields = ('url', 'state', 'flavor', 'hostname',
+                  'template', 'cloud', 'project')
         lookup_field = 'uuid'
         # TODO: Render ip addresses and volumes
+
+    def to_native(self, obj):
+        ret = super(InstanceSerializer, self).to_native(obj)
+        request = self.context['view'].request
+        additional_fields = ('environment', 'state', 'uptime',
+                             'flavor', 'IPs', 'hostname')
+        if request.user in obj.project.roles. \
+                get(role_type=Role.MANAGER).permission_group.user_set.all():
+            for k in ret.keys():
+                if k in additional_fields:
+                    del ret[k]
+        return ret
 
     def get_filtered_field_names(self):
         return 'project', 'flavor'
