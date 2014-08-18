4c1ad1b7a429b628d1aa50c21150f7810f83587a
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2014-08-18 11:37:27 +0300

    Fix setUp() separate attributes
    nc-50

--- nodeconductor/iaas/tests/test_purchases.py
+++ nodeconductor/iaas/tests/test_purchases.py
@@ -2,47 +2,45 @@ from rest_framework import test
 from rest_framework import status
 from rest_framework.reverse import reverse
 
-from nodeconductor.structure import models
 from nodeconductor.iaas.tests import factories as iaas_factories
+from nodeconductor.structure.models import Role
+from nodeconductor.structure.tests import factories as structure_factories
 
 
 class PurchasePermissionTest(test.APISimpleTestCase):
     def setUp(self):
-        self.purchases = iaas_factories.PurchaseFactory.create_batch(3)
+        self.user = structure_factories.UserFactory.create()
+        self.client.force_authenticate(user=self.user)
 
-        self.purchases[0].project.add_user(self.purchases[0].user,
-                                           models.Role.ADMINISTRATOR)
-        self.purchases[1].project.add_user(self.purchases[1].user,
-                                           models.Role.MANAGER)
+        admined_project = structure_factories.ProjectFactory()
+        managed_project = structure_factories.ProjectFactory()
+
+        admined_project.add_user(self.user, Role.ADMINISTRATOR)
+        managed_project.add_user(self.user, Role.MANAGER)
+
+        self.admined_purchase = iaas_factories.PurchaseFactory(project=admined_project)
+        self.managed_purchase = iaas_factories.PurchaseFactory(project=managed_project)
+        self.inaccessible_purchase = iaas_factories.PurchaseFactory()
 
     def test_user_can_list_purchase_history_of_project_he_is_administrator_of(self):
-        self.client.force_authenticate(user=self.purchases[0].user)
-
         response = self.client.get(reverse('purchase-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        purchase_url = self._get_purchase_url(self.purchases[0])
+        purchase_url = self._get_purchase_url(self.admined_purchase)
         self.assertIn(purchase_url, [purchase['url'] for purchase in response.data])
 
     def test_user_can_list_purchase_history_of_project_he_is_manager_of(self):
-        self.client.force_authenticate(user=self.purchases[1].user)
-
         response = self.client.get(reverse('purchase-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        purchase_url = self._get_purchase_url(self.purchases[1])
+        purchase_url = self._get_purchase_url(self.managed_purchase)
         self.assertIn(purchase_url, [purchase['url'] for purchase in response.data])
 
     def test_user_cannot_list_purchase_history_of_project_he_has_no_role_in(self):
-        self.client.force_authenticate(user=self.purchases[2].user)
-
         response = self.client.get(reverse('purchase-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        purchase_url = self._get_purchase_url(self.purchases[0])
-        self.assertNotIn(purchase_url, [purchase['url'] for purchase in response.data])
-
-        purchase_url = self._get_purchase_url(self.purchases[1])
+        purchase_url = self._get_purchase_url(self.inaccessible_purchase)
         self.assertNotIn(purchase_url, [purchase['url'] for purchase in response.data])
 
     # Helper methods
