579fb65aef6e0d8461d79db9c1cbf8e0dd46445d
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2014-08-18 12:36:26 +0300

    Add test purchases not allowed for any project
    nc-50

--- nodeconductor/iaas/tests/test_purchases.py
+++ nodeconductor/iaas/tests/test_purchases.py
@@ -14,14 +14,16 @@ class PurchasePermissionTest(test.APISimpleTestCase):
 
         admined_project = structure_factories.ProjectFactory()
         managed_project = structure_factories.ProjectFactory()
+        inaccessible_project = structure_factories.ProjectFactory()
 
         admined_project.add_user(self.user, Role.ADMINISTRATOR)
         managed_project.add_user(self.user, Role.MANAGER)
 
         self.admined_purchase = iaas_factories.PurchaseFactory(project=admined_project)
         self.managed_purchase = iaas_factories.PurchaseFactory(project=managed_project)
-        self.inaccessible_purchase = iaas_factories.PurchaseFactory()
+        self.inaccessible_purchase = iaas_factories.PurchaseFactory(project=inaccessible_project)
 
+    # List filtration tests
     def test_user_can_list_purchase_history_of_project_he_is_administrator_of(self):
         response = self.client.get(reverse('purchase-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -42,6 +44,15 @@ class PurchasePermissionTest(test.APISimpleTestCase):
 
         purchase_url = self._get_purchase_url(self.inaccessible_purchase)
         self.assertNotIn(purchase_url, [purchase['url'] for purchase in response.data])
+
+    def test_user_cannot_list_purchases_not_allowed_for_any_project(self):
+        inaccessible_purchase = iaas_factories.PurchaseFactory()
+
+        response = self.client.get(reverse('purchase-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        purchase_url = self._get_purchase_url(inaccessible_purchase)
+        self.assertNotIn(purchase_url, [instance['url'] for instance in response.data])
 
     # Helper methods
     def _get_purchase_url(self, purchase):
