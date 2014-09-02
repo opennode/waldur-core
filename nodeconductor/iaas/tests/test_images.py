a4b04127b2eed804806a68535d326c839fcdb72c
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2014-09-02 18:40:44 +0300

    Add creation, mutation, deletion tests, factory
    nc-2

--- nodeconductor/iaas/tests/test_images.py
+++ nodeconductor/iaas/tests/test_images.py
@@ -66,7 +66,7 @@ class ImagesApiPermissionTest(test.APISimpleTestCase):
         self.assertNotIn(image_url, [image['url'] for image in response.data])
 
     def test_user_cannot_list_images_not_allowed_for_any_project(self):
-        for user in self.users.keys():
+        for user in self.users.values():
             self._ensure_list_access_forbidden(user, 'inaccessible')
 
     # Direct image access tests
@@ -96,15 +96,72 @@ class ImagesApiPermissionTest(test.APISimpleTestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_user_cannot_access_image_not_allowed_for_any_project(self):
-        for user_role in self.users.keys():
+        for user_role in self.users.values():
             self._ensure_direct_access_forbidden(user_role, 'inaccessible')
+
+    # Creation tests
+    def test_anonymous_user_cannot_create_image(self):
+        for old_image in self.images.values():
+            new_image = iaas_factories.ImageFactory(cloud=old_image.cloud)
+            response = self.client.post(reverse('image-list'), self._get_valid_payload(new_image))
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authenticate_user_cannot_create_image(self):
+        for user in self.users.values():
+            self.client.force_authenticate(user=user)
+
+            for old_image in self.images.values():
+                new_image = iaas_factories.ImageFactory(cloud=old_image.cloud)
+                response = self.client.post(reverse('image-list'), self._get_valid_payload(new_image))
+                self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    # Mutation tests
+    def test_anonymous_user_cannot_change_image(self):
+        for image in self.images.values():
+            data = self._get_valid_payload(image)
+            response = self.client.put(self._get_image_url(image), data)
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_user_cannot_change_image_allowed_for_project_he_is_administrator_of(self):
+        self.client.force_authenticate(self.users['admin'])
+        image = self.images['admin']
+
+        data = self._get_valid_payload(image)
+        response = self.client.put(self._get_image_url(image), data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_user_cannot_change_image_allowed_for_project_he_is_manager_of(self):
+        self.client.force_authenticate(self.users['manager'])
+        image = self.images['manager']
+
+        data = self._get_valid_payload(image)
+        response = self.client.put(self._get_image_url(image), data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    # Deletion tests
+    def test_anonymous_user_cannot_delete_image(self):
+        for image in self.images.values():
+            response = self.client.delete(self._get_image_url(image))
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_user_cannot_delete_image_allowed_for_project_he_is_administrator_of(self):
+        self.client.force_authenticate(user=self.users['admin'])
+
+        response = self.client.delete(self._get_image_url(self.images['admin']))
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_user_cannot_delete_image_allowed_for_project_he_is_manager_of(self):
+        self.client.force_authenticate(user=self.users['manager'])
+
+        response = self.client.delete(self._get_image_url(self.images['manager']))
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     # Helper methods
     def _get_image_url(self, image):
         return 'http://testserver' + reverse('image-detail', kwargs={'uuid': image.uuid})
 
     def _ensure_list_access_forbidden(self, user_role, image):
-        self.client.force_authenticate(user=self.users[user_role])
+        self.client.force_authenticate(user=user_role)
 
         response = self.client.get(reverse('image-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -113,7 +170,17 @@ class ImagesApiPermissionTest(test.APISimpleTestCase):
         self.assertNotIn(image_url, [image['url'] for image in response.data])
 
     def _ensure_direct_access_forbidden(self, user_role, image):
-        self.client.force_authenticate(user=self.users[user_role])
+        self.client.force_authenticate(user=user_role)
 
         response = self.client.get(self._get_image_url(self.images[image]))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def _get_valid_payload(self, resource):
+        return {
+            'name': resource.name,
+            'cloud': 'http://testserver' + reverse('cloud-detail',
+                                                   kwargs={'uuid': resource.cloud.uuid}),
+            'architecture': resource.architecture,
+            'description': resource.description,
+            'license_type': resource.license_type,
+        }
