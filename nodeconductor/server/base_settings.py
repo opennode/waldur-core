e05564b61dd42312f5cb06f127d560c7ede2f9ae
Ihor Kaharlichenko <ihor@opennodecloud.com>
2014-09-02 15:40:25 +0400

    Enable token based auth, cease basic one
    So far the only way to obtain the token is to POST username/password.
    Other methods will be added separately.
    
    NC-83

--- nodeconductor/server/base_settings.py
+++ nodeconductor/server/base_settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'south',
     'background_task',
 
@@ -50,7 +51,7 @@ MIDDLEWARE_CLASSES = (
 REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
