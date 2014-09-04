9b669abb7747adb21da95f7a50dc655e8599145f
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2014-09-04 11:00:52 +0300

    Add manipulation tests
    nc-86

--- nodeconductor/structure/tests/test_customer.py
+++ nodeconductor/structure/tests/test_customer.py
@@ -73,28 +73,87 @@ class CustomerApiPermissionTest(UrlResolverMixin, test.APISimpleTestCase):
 
 class CustomerApiManipulationTest(UrlResolverMixin, test.APISimpleTestCase):
     def setUp(self):
-        self.user = factories.UserFactory()
-        self.client.force_authenticate(user=self.user)
+        self.users = {
+            'staff': factories.UserFactory(is_staff=True),
+            'owner': factories.UserFactory(),
+            'not_owner': factories.UserFactory(),
+        }
 
         self.customer = factories.CustomerFactory()
+
+        self.customer.add_user(self.users['owner'], CustomerRole.OWNER)
+
         self.customer_url = self._get_customer_url(self.customer)
 
-    def test_cannot_delete_customer(self):
+    # Deletion tests
+    def test_user_cannot_delete_customer_he_is_not_owner_of(self):
+        self.client.force_authenticate(user=self.users['not_owner'])
+
         response = self.client.delete(self.customer_url)
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_cannot_create_customer(self):
-        response = self.client.post(self.customer_url, self._get_valid_payload())
+    def test_user_cannot_delete_customer_he_is_owner_of(self):
+        self.client.force_authenticate(user=self.users['owner'])
+
+        response = self.client.delete(self.customer_url)
 
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    def test_cannot_change_customer_as_whole(self):
+    def test_user_cannot_delete_customer_if_he_is_staff(self):
+        self.client.force_authenticate(user=self.users['staff'])
+
+        response = self.client.delete(self.customer_url)
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    # Creation tests
+    def test_user_cannot_create_customer_he_is_not_owner_of(self):
+        self.client.force_authenticate(user=self.users['not_owner'])
+
+        response = self.client.post(reverse('customer-list'), self._get_valid_payload())
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_cannot_create_customer_he_is_owner_of(self):
+        self.client.force_authenticate(user=self.users['owner'])
+
+        response = self.client.post(reverse('customer-list'), self._get_valid_payload())
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_create_customer_if_he_is_staff(self):
+        self.client.force_authenticate(user=self.users['staff'])
+
+        response = self.client.post(reverse('customer-list'), self._get_valid_payload())
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # Mutation tests
+    def test_user_cannot_change_customer_as_whole_he_is_not_owner_of(self):
+        self.client.force_authenticate(user=self.users['not_owner'])
+
         response = self.client.put(self.customer_url, self._get_valid_payload(self.customer))
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_cannot_change_single_customer_field(self):
+    def test_user_cannot_change_customer_as_whole_he_is_owner_of(self):
+        self.client.force_authenticate(user=self.users['owner'])
+
+        response = self.client.put(self.customer_url, self._get_valid_payload(self.customer))
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_change_customer_as_whole_if_he_is_staff(self):
+        self.client.force_authenticate(user=self.users['staff'])
+
+        response = self.client.put(self.customer_url, self._get_valid_payload(self.customer))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_change_single_customer_field_he_is_not_owner_of(self):
+        self.client.force_authenticate(user=self.users['not_owner'])
+
         payload = self._get_valid_payload(self.customer)
 
         for field, value in payload.items():
@@ -106,6 +165,35 @@ class CustomerApiManipulationTest(UrlResolverMixin, test.APISimpleTestCase):
 
             self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_user_cannot_change_single_customer_field_he_is_owner_of(self):
+        self.client.force_authenticate(user=self.users['owner'])
+
+        payload = self._get_valid_payload(self.customer)
+
+        for field, value in payload.items():
+            data = {
+                field: value
+            }
+
+            response = self.client.patch(self.customer_url, data)
+
+            self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_change_single_customer_field_if_he_is_staff(self):
+        self.client.force_authenticate(user=self.users['staff'])
+
+        payload = self._get_valid_payload(self.customer)
+
+        for field, value in payload.items():
+            data = {
+                field: value
+            }
+
+            response = self.client.patch(self.customer_url, data)
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # Helper methods
     def _get_valid_payload(self, resource=None):
         resource = resource or factories.CustomerFactory()
 
