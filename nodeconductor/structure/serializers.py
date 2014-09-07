c9f6cda9e2e32732296ef3998d7302d986ae9248
Ilja Livenson <ilja.livenson@gmail.com>
2014-09-07 09:31:10 +0400

    Extended project list with project_group information

--- nodeconductor/structure/serializers.py
+++ nodeconductor/structure/serializers.py
@@ -12,19 +12,22 @@ from nodeconductor.structure import models
 
 User = auth.get_user_model()
 
-class ProjectSerializer(serializers.HyperlinkedModelSerializer):
-    customer_name = serializers.Field(source='customer.name')
-
-    class Meta(object):
-        model = models.Project
-        fields = ('url', 'name', 'customer', 'customer_name')
-        lookup_field = 'uuid'
 
 class BasicProjectGroupSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta(object):
         model = models.ProjectGroup
         fields = ('url', 'name', 'customer')
+        lookup_field = 'uuid'
+
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    customer_name = serializers.Field(source='customer.name')
+    project_groups = BasicProjectGroupSerializer(many=True, read_only=True)
+
+    class Meta(object):
+        model = models.Project
+        fields = ('url', 'name', 'customer', 'customer_name', 'project_groups')
         lookup_field = 'uuid'
 
 
