450c8476efba509aec7327b4fd7d4d535c53e43c
Ihor Kaharlichenko <ihor@opennodecloud.com>
2014-09-28 12:20:37 +0400

    Provide version info at runtime
    The version is retrieved from package metadata in production.
    For development the version is taken from git.
    
    The version string strives to be compatible with PEP-440
    
    NC-44

---
+++ nodeconductor/__init__.py
@@ -0,0 +1,41 @@
+# The dancing with the function and its deletion is done
+# to keep the namespace clean: only __version__ is going to be exposed.
+
+
+def _get_version():
+    import pkg_resources
+
+    # Based on http://stackoverflow.com/a/17638236/175349
+    # and https://github.com/pwaller/__autoversion__/blob/master/__autoversion__.py
+
+    try:
+        return pkg_resources.get_distribution('nodeconductor').version
+    except pkg_resources.DistributionNotFound:
+        import os.path
+        import re
+        import subprocess
+
+        repo_dir = os.path.join(os.path.dirname(__file__), os.path.pardir)
+
+        try:
+            with open(os.devnull, 'w') as DEV_NULL:
+                description = subprocess.check_output(
+                    ['git', 'describe', '--tags', '--dirty=.dirty'],
+                    cwd=repo_dir, stderr=DEV_NULL
+                ).strip()
+
+            v = re.search(r'-[0-9]+-', description)
+            if v is not None:
+                # Replace -n- with -branchname-n-
+                # branch = r"-{0}-\1-".format(cls.get_branch(path))
+                description, _ = re.subn('-([0-9]+)-', r'+\1.', description, 1)
+
+            if description[0] == 'v':
+                description = description[1:]
+
+            return description
+        except (OSError, subprocess.CalledProcessError):
+            return 'unknown'
+
+__version__ = _get_version()
+del _get_version
