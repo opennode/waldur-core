b3820b694300f3e1a8c0d71c3c36f984001fa168
Pavel <marchukpavelp@gmail.com>
2014-10-07 13:06:02 +0300

    backup model tests

--- nodeconductor/backup/tests/test_models.py
+++ nodeconductor/backup/tests/test_models.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 from datetime import timedelta
+from mock import patch
 
+from django.db import IntegrityError
 from django.test import TestCase
 from django.utils import timezone
 
@@ -83,3 +85,90 @@ class BackupScheduleTest(TestCase):
         self.assertEqual(future_schedule.backups.count(), 0)
         self.assertEqual(schedule_for_execution.backups.count(), 1)
         self.assertEqual(models.Backup.objects.get(pk=expired_backup.pk).state, models.Backup.States.DELETING)
+
+
+class BackupTest(TestCase):
+
+    mocked_task_result = type(str('MockedTaskResult'), (object, ), {'id': 'result_id'})
+
+    class MockedAsyncResult(object):
+
+        def __init__(self, ready):
+            self._ready = ready
+
+        def ready(self):
+            return self._ready
+
+    def test_save(self):
+        backup = factories.BackupFactory()
+        with self.assertRaises(IntegrityError):
+            backup.save()
+
+    @patch('nodeconductor.backup.tasks.backup_task.delay', return_value=mocked_task_result)
+    def test_start_backup(self, mocked_task):
+        backup = factories.BackupFactory()
+        backup.start_backup()
+        mocked_task.assert_called_with(backup.backup_source)
+        self.assertEqual(backup.result_id, BackupTest.mocked_task_result().id)
+        self.assertEqual(backup.state, models.Backup.States.BACKUPING)
+
+    @patch('nodeconductor.backup.tasks.restore_task.delay', return_value=mocked_task_result)
+    def test_start_restore(self, mocked_task):
+        backup = factories.BackupFactory()
+        backup.start_restore()
+        mocked_task.assert_called_with(backup.backup_source)
+        self.assertEqual(backup.result_id, BackupTest.mocked_task_result().id)
+        self.assertEqual(backup.state, models.Backup.States.RESTORING)
+
+    @patch('nodeconductor.backup.tasks.delete_task.delay', return_value=mocked_task_result)
+    def test_start_delete(self, mocked_task):
+        backup = factories.BackupFactory()
+        backup.start_delete()
+        mocked_task.assert_called_with(backup.backup_source)
+        self.assertEqual(backup.result_id, BackupTest.mocked_task_result().id)
+        self.assertEqual(backup.state, models.Backup.States.DELETING)
+
+    def test_verify_backup(self):
+        with patch('nodeconductor.backup.tasks.backup_task.AsyncResult',
+                   return_value=self.MockedAsyncResult(True)) as mocked_result:
+            backup = factories.BackupFactory(result_id='result_id', state=models.Backup.States.BACKUPING)
+            backup.verify_backup()
+            self.assertEqual(backup.state, models.Backup.States.READY)
+            mocked_result.assert_called_with(backup.result_id)
+
+        with patch('nodeconductor.backup.tasks.backup_task.AsyncResult',
+                   return_value=self.MockedAsyncResult(False)) as mocked_result:
+            backup = factories.BackupFactory(result_id='result_id', state=models.Backup.States.BACKUPING)
+            backup.verify_backup()
+            self.assertEqual(backup.state, models.Backup.States.BACKUPING)
+            mocked_result.assert_called_with(backup.result_id)
+
+    def test_verify_restore(self):
+        with patch('nodeconductor.backup.tasks.restore_task.AsyncResult',
+                   return_value=self.MockedAsyncResult(True)) as mocked_result:
+            backup = factories.BackupFactory(result_id='result_id', state=models.Backup.States.RESTORING)
+            backup.verify_restore()
+            self.assertEqual(backup.state, models.Backup.States.READY)
+            mocked_result.assert_called_with(backup.result_id)
+
+        with patch('nodeconductor.backup.tasks.restore_task.AsyncResult',
+                   return_value=self.MockedAsyncResult(False)) as mocked_result:
+            backup = factories.BackupFactory(result_id='result_id', state=models.Backup.States.RESTORING)
+            backup.verify_restore()
+            self.assertEqual(backup.state, models.Backup.States.RESTORING)
+            mocked_result.assert_called_with(backup.result_id)
+
+    def test_verify_delete(self):
+        with patch('nodeconductor.backup.tasks.delete_task.AsyncResult',
+                   return_value=self.MockedAsyncResult(True)) as mocked_result:
+            backup = factories.BackupFactory(result_id='result_id', state=models.Backup.States.DELETING)
+            backup.verify_delete()
+            self.assertEqual(backup.state, models.Backup.States.DELETED)
+            mocked_result.assert_called_with(backup.result_id)
+
+        with patch('nodeconductor.backup.tasks.delete_task.AsyncResult',
+                   return_value=self.MockedAsyncResult(False)) as mocked_result:
+            backup = factories.BackupFactory(result_id='result_id', state=models.Backup.States.DELETING)
+            backup.verify_delete()
+            self.assertEqual(backup.state, models.Backup.States.DELETING)
+            mocked_result.assert_called_with(backup.result_id)
