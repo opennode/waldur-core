ae6c2b0d54935235d72eedf202d8b37cdb1d7a30
Juri Hudolejev <jhudolejev@gmail.com>
2014-10-09 10:57:12 +0400

    Django REST Swagger app removed (NC-125)

--- nodeconductor/server/base_settings.py
+++ nodeconductor/server/base_settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = (
 
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_swagger',
     'south',
 
     'permission',
--- nodeconductor/server/urls.py
+++ nodeconductor/server/urls.py
@@ -31,7 +31,6 @@ urlpatterns = patterns(
     url(r'^api-auth/saml2/', 'nodeconductor.core.views.assertion_consumer_service'),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
 )
 
 # FIXME: This shouldn't be here
