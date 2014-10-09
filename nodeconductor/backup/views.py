180c2bd205001ea8dcf9cdfaf128a13e3adbe2c2
Pavel <marchukpavelp@gmail.com>
2014-10-09 09:58:00 +0300

    backup views rewrited

--- nodeconductor/backup/views.py
+++ nodeconductor/backup/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import permissions as rf_permissions
+from rest_framework import views
+from rest_framework.response import Response
 
 from nodeconductor.core import viewsets
 from nodeconductor.backup import models
@@ -7,7 +11,7 @@ from nodeconductor.structure import filters as structure_filters
 
 
 class BackupScheduleViewSet(viewsets.ModelViewSet):
-    model = models.BackupSchedule
+    queryset = models.Backup.objects.all()
     serializer_class = serializers.BackupScheduleSerializer
     lookup_field = 'uuid'
     filter_backends = (structure_filters.GenericRoleFilter,)
@@ -15,10 +19,30 @@ class BackupScheduleViewSet(viewsets.ModelViewSet):
                           rf_permissions.DjangoObjectPermissions)
 
 
-class BackupViewSet(viewsets.ModelViewSet):
-    model = models.Backup
+class BackupViewSet(viewsets.CreateModelViewSet):
+    queryset = models.Backup.objects.all()
     serializer_class = serializers.BackupSerializer
     lookup_field = 'uuid'
     filter_backends = (structure_filters.GenericRoleFilter,)
     permission_classes = (rf_permissions.IsAuthenticated,
                           rf_permissions.DjangoObjectPermissions)
+
+    def get_object(self):
+        """
+        When backup is created manually - it starts backuping
+        """
+        backup = super(viewsets.CreateModelViewSet, self).get_object()
+        backup.start_backup()
+        return backup
+
+
+class BackupOperationView(views.APIView):
+
+    def post(self, request, uuid, action):
+        backup = get_object_or_404(models.Backup, uuid=uuid)
+        if action == 'restore':
+            replace_original = request.POST.get('replace_original', False)
+            backup.start_restoration(replace_original=replace_original)
+        elif action == 'delete':
+            backup.start_deletion()
+        return Response({'backup_state': backup.state}, status=200)
