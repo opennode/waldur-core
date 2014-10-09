a8957b0eed3ee7e52c6cb9ec7dd305edbd018307
Pavel <marchukpavelp@gmail.com>
2014-10-09 19:12:03 +0300

    BackupPermissionFilter

--- nodeconductor/backup/views.py
+++ nodeconductor/backup/views.py
@@ -1,13 +1,34 @@
+from __future__ import unicode_literals
+
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
+from django.contrib.contenttypes import models as ct_models
 
 from rest_framework import permissions as rf_permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
 from nodeconductor.core import viewsets
-from nodeconductor.backup import models
-from nodeconductor.backup import serializers
+from nodeconductor.backup import models, serializers, backup_registry
 from nodeconductor.structure import filters as structure_filters
+
+
+class BackupPermissionFilter():
+
+    def _get_user_visible_model_instances_ids(self, user, model):
+        queryset = structure_filters.filter_queryset_for_user(model.objects.all(), user)
+        return queryset.values_list('pk', flat=True)
+
+    def filter_queryset(self, request, queryset, view):
+        """
+        Filter backups with source to which user has view access
+        """
+        q_query = Q()
+        for model in backup_registry.get_backupable_models():
+            model_content_type = ct_models.ContentType.objects.get_for_model(model)
+            instances_ids = self._get_user_visible_model_instances_ids(request.user, model)
+            q_query |= (Q(content_type=model_content_type) & Q(object_id__in=instances_ids))
+        return queryset.filter(q_query)
 
 
 class BackupScheduleViewSet(viewsets.ModelViewSet):
