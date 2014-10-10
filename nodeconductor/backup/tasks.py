01d25302ba3293df062db3238d0d666a11f950b8
Pavel <marchukpavelp@gmail.com>
2014-10-10 16:59:13 +0300

    backup tasks corrected

--- nodeconductor/backup/tasks.py
+++ nodeconductor/backup/tasks.py
@@ -3,14 +3,14 @@ from celery import shared_task
 
 @shared_task
 def backup_task(backupable_instance):
-    backupable_instance.get_backup_strategy.backup()
+    backupable_instance.get_backup_strategy().backup()
 
 
 @shared_task
 def restoration_task(backupable_instance, replace_original=False):
-    backupable_instance.get_backup_strategy.restore(replace_original)
+    backupable_instance.get_backup_strategy().restore(replace_original)
 
 
 @shared_task
 def deletion_task(backupable_instance):
-    backupable_instance.get_backup_strategy.delete()
+    backupable_instance.get_backup_strategy().delete()
