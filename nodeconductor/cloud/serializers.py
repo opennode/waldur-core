c368353755dd47b929b2e719c12e660a893e816a
Pavel <marchukpavelp@gmail.com>
2014-10-16 19:18:13 +0300

    CloudSerializer methods

--- nodeconductor/cloud/serializers.py
+++ nodeconductor/cloud/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from nodeconductor.core import serializers as core_serializers
 from nodeconductor.cloud import models
 from nodeconductor.structure.serializers import BasicProjectSerializer
+from nodeconductor.structure import models as structure_models
 
 
 class BasicCloudSerializer(core_serializers.BasicInfoSerializer):
@@ -32,6 +33,20 @@ class CloudSerializer(core_serializers.PermissionFieldFilteringMixin,
         model = models.Cloud
         fields = ('uuid', 'url', 'name', 'customer', 'customer_name', 'flavors', 'projects')
         lookup_field = 'uuid'
+
+    public_fields = ('uuid', 'url', 'name')
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs['context'].get('user', None)
+        super(CloudSerializer, self).__init__(*args, **kwargs)
+
+    def get_fields(self):
+        fields = super(CloudSerializer, self).get_fields()
+        is_customer_owner = self.object.customer.roles.filter(
+            permission_group__user=self.user, role_type=structure_models.CustomerRole.OWNER).exists()
+        if self.user is not None and not self.user.is_superuser and not is_customer_owner:
+            return dict((key, value) for key, value in fields.iteritems() if key in self.public_fields)
+        return fields
 
     def get_filtered_field_names(self):
         return 'customer',
