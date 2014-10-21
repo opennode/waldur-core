eeb811a2df8abc2158cc77fa262fd228bef36966
Pavel <marchukpavelp@gmail.com>
2014-10-21 16:47:54 +0300

    view action test

--- nodeconductor/iaas/tests/test_views.py
+++ nodeconductor/iaas/tests/test_views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 
 from nodeconductor.structure.tests import factories as structure_factories
 from nodeconductor.iaas.tests import factories
-from nodeconductor.iaas import views
+from nodeconductor.iaas import views, models
 from nodeconductor.cloud.tests import factories as cloud_factories
 
 
@@ -59,3 +59,31 @@ class LicenseViewSetTest(TestCase):
         # user is not staff
         mocked_request.user = structure_factories.UserFactory()
         self.assertRaises(Http404, lambda: self.view.get_queryset())
+
+
+class TemplateViewSetTest(TestCase):
+
+    def setUp(self):
+        self.view = views.TemplateViewSet()
+
+    def test_licenses(self):
+        template = factories.TemplateFactory()
+        # as regular user:
+        user = structure_factories.UserFactory()
+        mocked_request = type(str('MockedRequest'), (object,), {'user': user, 'DATA': {}})
+        self.assertRaises(Http404, lambda: self.view.licenses(mocked_request, str(template.uuid)))
+        # as staff:
+        staff = structure_factories.UserFactory(is_staff=True)
+        mocked_request.user = staff
+        # add licenses to template:
+        licenses = [factories.LicenseFactory() for i in range(5)]
+        mocked_request.DATA['licenses'] = [str(l.uuid) for l in licenses]
+        self.view.licenses(mocked_request, str(template.uuid))
+        template = models.Template.objects.get(pk=template.pk)
+        self.assertSequenceEqual(template.licenses.all(), licenses)
+        # remove licenses from template:
+        licenses = licenses[:1]
+        mocked_request.DATA['licenses'] = [str(l.uuid) for l in licenses]
+        self.view.licenses(mocked_request, str(template.uuid))
+        template = models.Template.objects.get(pk=template.pk)
+        self.assertSequenceEqual(template.licenses.all(), licenses)
