cbf25d94f8e1bd9d020ed5486d39d96f0a9cf569
Pavel <marchukpavelp@gmail.com>
2014-10-22 09:16:01 +0300

    LicenseViewSet permissions corrected

--- nodeconductor/iaas/tests/test_views.py
+++ nodeconductor/iaas/tests/test_views.py
@@ -56,6 +56,3 @@ class LicenseViewSetTest(TestCase):
         self.view.request = mocked_request
         queryset = self.view.get_queryset()
         self.assertSequenceEqual(list(queryset), [])
-        # user is not staff
-        mocked_request.user = structure_factories.UserFactory()
-        self.assertRaises(Http404, lambda: self.view.get_queryset())
