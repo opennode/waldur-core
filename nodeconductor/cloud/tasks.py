77c1aa3f92a0f58c6f724c6bb3d0311aced79b7f
Pavel <marchukpavelp@gmail.com>
2014-10-27 09:20:04 +0200

    task for tenant creation added

---
+++ nodeconductor/cloud/tasks.py
@@ -0,0 +1,18 @@
+from celery import shared_task
+from keystoneclient.v2_0 import client
+
+from nodeconductor.cloud import models
+
+# XXX: this have to be moved to settings or implemented in other way
+ADMIN_TENANT = 'admin'
+
+
+@shared_task
+def connect_project_to_cloud(cloud, project):
+    """
+    Connect project to cloud, add new tenant and store its uuid in membership model
+    """
+    keystone = client.Client(cloud.username, cloud.password, ADMIN_TENANT, auth_url=cloud.auth_url)
+    tenant = keystone.tenants.create(tenant_name=project.name, description=project.description, enabled=True)
+    models.CloudProjectMembership.objectes.create(
+        cloud=cloud, project=project, tenant_uuid=tenant.id)
