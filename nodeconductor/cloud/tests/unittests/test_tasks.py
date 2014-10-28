bec7b1928f2ec7382a2764494541b443f9b4abd3
Pavel <marchukpavelp@gmail.com>
2014-10-28 11:00:55 +0200

    tasks tests

--- nodeconductor/cloud/tests/unittests/test_tasks.py
+++ nodeconductor/cloud/tests/unittests/test_tasks.py
@@ -3,14 +3,22 @@ from mock import patch
 
 from nodeconductor.cloud import models, tasks
 from nodeconductor.cloud.tests import factories, helpers
-from nodeconductor.structure.tests import factories as structure_factories
 
 
 class TestTasks(TestCase):
 
-    def test_connect_project_to_cloud(self):
-        project = structure_factories.ProjectFactory()
-        cloud = factories.CloudFactory()
-        with patch('nodeconductor.cloud.tasks.client.Client', return_value=helpers.KeystoneMockedClient):
-            tasks.connect_project_to_cloud(project=project, cloud=cloud)
-            self.assertEqual(models.CloudProjectMembership.objects.filter(project=project, cloud=cloud).count(), 1)
+    def test_connect_project_to_cloud_success(self):
+        membership = factories.CloudProjectMembershipFactory()
+        with patch('nodeconductor.cloud.models.client.Client', return_value=helpers.KeystoneMockedClient):
+            tasks.create_backend_membership(membership)
+            self.assertEqual(
+                models.CloudProjectMembership.objects.get(pk=membership.pk).state,
+                models.CloudProjectMembership.States.READY)
+
+    def test_connect_project_to_cloud_error(self):
+        membership = factories.CloudProjectMembershipFactory()
+        with patch('nodeconductor.cloud.models.client.Client', side_effect=Exception()):
+            tasks.create_backend_membership(membership)
+            self.assertEqual(
+                models.CloudProjectMembership.objects.get(pk=membership.pk).state,
+                models.CloudProjectMembership.States.ERRED)
