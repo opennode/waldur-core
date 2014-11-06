86eae2851e05ac553b8c9ed5ccf17dc8b489238b
Pavel <marchukpavelp@gmail.com>
2014-11-06 16:09:24 +0200

    test that user does not receive deleted backups + separate file for security groups tests (ITACLOUD-2362)

--- nodeconductor/backup/tests/factories.py
+++ nodeconductor/backup/tests/factories.py
@@ -1,5 +1,7 @@
 import factory
 
+from django.core.urlresolvers import reverse
+
 from nodeconductor.backup import models
 from nodeconductor.cloud.tests.factories import FlavorFactory
 
@@ -21,3 +23,13 @@ class BackupFactory(factory.DjangoModelFactory):
 
     backup_schedule = factory.SubFactory(BackupScheduleFactory)
     backup_source = factory.LazyAttribute(lambda b: b.backup_schedule.backup_source)
+
+    @classmethod
+    def get_url(self, backup):
+        if backup is None:
+            backup = BackupFactory()
+        return 'http://testserver' + reverse('backup-detail', kwargs={'uuid': backup.uuid})
+
+    @classmethod
+    def get_list_url(self):
+        return 'http://testserver' + reverse('backup-list')
