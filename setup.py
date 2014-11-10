102f8de556f189db42ef6afcb938931fb1e9a34f
Juri Hudolejev <jhudolejev@gmail.com>
2014-11-10 23:02:51 +0400

    Started development of new version: 0.9.0.dev0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ install_requires = [
 
 setup(
     name='nodeconductor',
-    version='0.8.0',
+    version='0.9.0.dev0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/nodeconductor',
