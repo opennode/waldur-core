7ee3f7bef50dc8a029dcd445be008017af0b0965
Ihor Kaharlichenko <ihor@opennodecloud.com>
2014-11-13 12:02:44 +0400

    Show connected images for template in admin
    NC-168

--- nodeconductor/iaas/admin.py
+++ nodeconductor/iaas/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 from nodeconductor.iaas import models
 
 
+# Inspired by Django Snippet https://djangosnippets.org/snippets/2629/
+class ReadonlyInlineMixin(object):
+    can_delete = False
+    extra = 0
+
+    def has_add_permission(self, request):
+        return False
+
+
 class InstanceAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         if obj:
@@ -18,9 +27,25 @@ class PurchaseAdmin(admin.ModelAdmin):
     readonly_fields = ('date', 'user', 'project')
 
 
+class ImageInline(ReadonlyInlineMixin, admin.TabularInline):
+    model = models.Image
+    fields = ('get_cloud_name', 'get_customer_name', 'backend_id')
+    readonly_fields = ('get_cloud_name', 'get_customer_name', 'backend_id')
+    ordering = ('cloud__name', 'cloud__customer__name')
+    verbose_name_plural = 'Connected cloud images'
+
+    def get_cloud_name(self, obj):
+        return obj.cloud.name
+    get_cloud_name.short_description = 'Cloud'
+
+    def get_customer_name(self, obj):
+        return obj.cloud.customer.name
+    get_customer_name.short_description = 'Customer'
+
+
 class TemplateMappingInline(admin.TabularInline):
     model = models.TemplateMapping
-    fields = ('description', 'backend_id')
+    fields = ('description', 'backend_image_id')
     ordering = ('description', )
     extra = 3
 
@@ -28,11 +53,11 @@ class TemplateMappingInline(admin.TabularInline):
 class TemplateAdmin(admin.ModelAdmin):
     inlines = (
         TemplateMappingInline,
+        ImageInline,
     )
     ordering = ('name', )
 
 
 admin.site.register(models.Instance, InstanceAdmin)
 admin.site.register(models.Template, TemplateAdmin)
-admin.site.register(models.Image)
 admin.site.register(models.Purchase, PurchaseAdmin)
