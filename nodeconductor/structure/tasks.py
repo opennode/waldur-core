c8180a8e768f5b6572c5efdc1e401cd84a51ec5a
Pavel <marchukpavelp@gmail.com>
2014-11-24 11:30:56 +0200

    ZabbixApiClient initialization in tasks corrected (nc-195)

--- nodeconductor/structure/tasks.py
+++ nodeconductor/structure/tasks.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals
 
 import logging
 
-from django.conf import settings
 from celery import shared_task
 
 from nodeconductor.monitoring.zabbix.api_client import ZabbixApiClient
@@ -13,11 +12,11 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def create_zabbix_hostgroup(project):
-    zabbix_client = ZabbixApiClient(settings.NODECONDUCTOR['MONITORING']['ZABBIX'])
+    zabbix_client = ZabbixApiClient()
     zabbix_client.create_hostgroup(project)
 
 
 @shared_task
 def delete_zabbix_hostgroup(project):
-    zabbix_client = ZabbixApiClient(settings.NODECONDUCTOR['MONITORING']['ZABBIX'])
+    zabbix_client = ZabbixApiClient()
     zabbix_client.delete_hostgroup(project)
