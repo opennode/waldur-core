60a22d0eb7b9f5eae176dda1e7c0df834cfa72ac
Pavel <marchukpavelp@gmail.com>
2014-12-09 17:43:16 +0200

    unittest for Instance model _copy methods (nc-178)

--- nodeconductor/iaas/tests/unittests/test_models.py
+++ nodeconductor/iaas/tests/unittests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from mock import Mock
 
 from nodeconductor.iaas.tests import factories
 from nodeconductor.structure.tests import factories as structure_factories
@@ -45,3 +46,111 @@ class InstanceTest(TestCase):
         self.assertEqual(instance_license.template_license, template_license)
         self.assertEqual(instance_license.setup_fee, template_license.setup_fee)
         self.assertEqual(instance_license.monthly_fee, template_license.monthly_fee)
+
+
+class TestInstanceCopySshPublicKeysAttribute(TestCase):
+
+    def setUp(self):
+        # given
+        self.instance = factories.InstanceFactory.create()
+        self.ssh_public_key = factories.SshPublicKeyFactory.build()
+
+    def when(self):
+        self.instance.ssh_public_key = self.ssh_public_key
+        self.instance._copy_ssh_public_key_attributes()
+
+    def test_ssh_public_key_name_is_copied(self):
+        self.when()
+        # then
+        self.assertEqual(self.instance.ssh_public_key_name, self.ssh_public_key.name)
+
+    def test_ssh_public_key_fingerprint_is_copied(self):
+        self.when()
+        # then
+        self.assertEqual(self.instance.ssh_public_key_fingerprint, self.ssh_public_key.fingerprint)
+
+
+class TestInstanceCopyFlavorAttributes(TestCase):
+
+    def setUp(self):
+        # given
+        self.instance = factories.InstanceFactory.create()
+        self.flavor = factories.FlavorFactory.build()
+
+    def when(self):
+        self.instance.flavor = self.flavor
+        self.instance._copy_flavor_attributes()
+
+    def test_flavor_ram_is_copied(self):
+        self.when()
+        # then
+        self.assertEqual(self.instance.ram, self.flavor.ram)
+
+    def test_flavor_disk_is_copied(self):
+        self.when()
+        # then
+        self.assertEqual(self.instance.system_volume_size, self.flavor.disk)
+
+    def test_flavor_cores_is_copied(self):
+        self.when()
+        # then
+        self.assertEqual(self.instance.cores, self.flavor.cores)
+
+    def test_flavor_cloud_is_copied(self):
+        self.when()
+        # then
+        self.assertEqual(self.instance.cloud, self.flavor.cloud)
+
+
+class TestInstanceCopyTemplateAttributes(TestCase):
+
+    def setUp(self):
+        # given
+        self.instance = factories.InstanceFactory.create()
+        self.template = factories.TemplateFactory.build()
+
+    def when(self):
+        self.instance.template = self.template
+        self.instance._copy_template_attributes()
+
+    def test_flavor_ram_is_copied(self):
+        self.when()
+        # then
+        self.assertEqual(self.instance.agreed_sla, self.template.sla_level)
+
+
+class TestInstanceSave(TestCase):
+
+    def setUp(self):
+        # given
+        self.instance = factories.InstanceFactory.create()
+        self.instance._copy_template_attributes = Mock()
+        self.instance._init_instance_licenses = Mock()
+        self.instance._copy_flavor_attributes = Mock()
+        self.instance._copy_ssh_public_key_attributes = Mock()
+
+    def when(self, create):
+        if create:
+            self.instance.uuid = None
+            self.instance.pk = None
+        self.instance.save()
+
+    def test_copy_template_attributes_was_called_if_instance_was_created(self):
+        self.when(create=True)
+        #then
+        self.instance._copy_template_attributes.assert_called_once_with()
+
+    def test_copy_flavor_attributes_was_called_if_instance_was_created(self):
+        self.when(create=True)
+        #then
+        self.instance._copy_flavor_attributes.assert_called_once_with()
+
+    def test_copy_ssh_public_key_attributes_was_called_if_instance_was_created(self):
+        self.when(create=True)
+        #then
+        self.instance._copy_ssh_public_key_attributes.assert_called_once_with()
+
+    def test_init_instance_licenses_was_called_if_instance_was_created(self):
+        self.when(create=True)
+        #then
+        self.instance._init_instance_licenses.assert_called_once_with()
