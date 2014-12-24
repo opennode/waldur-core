db59327ae7b41817cca5aa3d1b1ed343193144d5
Juri Hudolejev <jhudolejev@gmail.com>
2014-12-24 10:56:50 +0400

    Preparing new release: 0.13.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ install_requires = [
 
 setup(
     name='nodeconductor',
-    version='0.12.0.dev0',
+    version='0.13.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/nodeconductor',
