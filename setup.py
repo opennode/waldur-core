6887b1acfc011e01585d82af6ce3dd616b900c59
Ihor Kaharlichenko <ihor@opennodecloud.com>
2014-12-26 23:09:19 +0400

    Starting new development cycle: 0.17

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ install_requires = [
 
 setup(
     name='nodeconductor',
-    version='0.16.1',
+    version='0.17.0.dev0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/nodeconductor',
