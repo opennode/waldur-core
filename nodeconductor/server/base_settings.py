a3622f33786d34d6d877ab955181ce2d9482063d
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2014-12-29 16:37:21 +0200

    Remove unnecessary static_dir, modify template
    NC-182

--- nodeconductor/server/base_settings.py
+++ nodeconductor/server/base_settings.py
@@ -109,10 +109,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'nodeconductor/core', 'static'),
-)
-
 SAML_CREATE_UNKNOWN_USER = True
 
 BROKER_URL = 'redis://localhost'
