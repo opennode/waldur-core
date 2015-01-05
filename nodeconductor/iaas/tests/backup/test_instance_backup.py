0ec4824891e1f807fdbc2ebbde4f5358c0dc5409
Pavel <marchukpavelp@gmail.com>
2015-01-05 17:23:18 +0200

    rewrite backup strategy tests (nc-286)

--- nodeconductor/iaas/tests/backup/test_instance_backup.py
+++ nodeconductor/iaas/tests/backup/test_instance_backup.py
@@ -5,40 +5,56 @@ from mock import Mock
 from django.utils import unittest
 
 from nodeconductor.iaas.backup.instance_backup import InstanceBackupStrategy
+from nodeconductor.iaas.tests import factories
 
 
 class InstanceBackupStrategyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.backup_ids = ['a52ef740-8dfa-4a26-87d5-3b5bb095681d', 'c695e654-d6a4-4202-b1b9-eb1e66aa43a5']
-        self.additional_data = ','.join(self.backup_ids)
-        self.instance = Mock()
-        self.restored_vm = Mock()
-        self.restored_vm.id = 2
+        self.instance = factories.InstanceFactory()
+        self.flavor = factories.FlavorFactory()
+        self.key = factories.SshPublicKeyFactory()
+        self.copied_system_volume_id = '350b81e1-f991-401c-99b1-ebccc5a517a6'
+        self.copied_data_volume_id = 'dba9b361-277c-46b2-99ca-1136b3eba6ed'
+        self.additional_data = InstanceBackupStrategy._get_instance_addition_data(self.instance)
+        self.additional_data['system_volume_id'] = self.copied_system_volume_id
+        self.additional_data['data_volume_id'] = self.copied_data_volume_id
 
         self.mocked_backed = Mock()
         InstanceBackupStrategy._get_backend = Mock(return_value=self.mocked_backed)
-        self.mocked_backed.backup_instance = Mock(return_value=self.backup_ids)
-        self.mocked_backed.restore_instance = Mock(return_value=self.restored_vm)
+        self.mocked_backed.copy_volumes = Mock(return_value=[self.copied_system_volume_id, self.copied_data_volume_id])
 
     def test_strategy_backup_method_calls_backend_backup_instance_method(self):
         InstanceBackupStrategy.backup(self.instance)
-        self.mocked_backed.backup_instance.assert_called_once_with(self.instance)
+        self.mocked_backed.copy_volumes.assert_called_once_with(
+            membership=self.instance.cloud_project_membership,
+            volume_ids=[self.instance.system_volume_id, self.instance.data_volume_id]
+        )
 
     def test_strategy_backup_method_returns_backups_ids_as_string(self):
         result = InstanceBackupStrategy.backup(self.instance)
-        expected = ','.join(self.backup_ids)
+        expected = InstanceBackupStrategy._get_instance_addition_data(self.instance)
+        expected['system_volume_id'] = self.copied_system_volume_id
+        expected['data_volume_id'] = self.copied_data_volume_id
         self.assertEqual(result, expected)
 
     def test_strategy_restore_method_calls_backend_restore_instance_method(self):
-        InstanceBackupStrategy.restore(self.instance, self.additional_data)
-        self.mocked_backed.restore_instance.assert_called_once_with(self.instance, self.backup_ids)
+        InstanceBackupStrategy.restore(self.instance, self.additional_data, self.key, self.flavor, 'new_hostname')
+        self.mocked_backed.copy_volumes.assert_called_once_with(
+            membership=self.instance.cloud_project_membership,
+            volume_ids=[self.copied_system_volume_id, self.copied_data_volume_id],
+            prefix='Restored volume',
+        )
 
-    def test_strategy_restore_method_replace_instance_backend_id(self):
-        InstanceBackupStrategy.restore(self.instance, self.additional_data)
-        self.assertEqual(self.instance.backend_id, self.restored_vm.id)
-        self.instance.save.assert_called_once_with()
+    def test_strategy_restore_method_creates_new_instance(self):
+        new_instance = InstanceBackupStrategy.restore(
+            self.instance, self.additional_data, self.key, self.flavor, 'new_hostname')
+        self.assertEqual(new_instance.hostname, 'new_hostname')
+        self.assertNotEqual(new_instance.id, self.instance.id)
 
     def test_strategy_delete_method_calls_backend_delete_instance_method(self):
         InstanceBackupStrategy.delete(self.instance, self.additional_data)
-        self.mocked_backed.delete_instance_backup.assert_called_once_with(self.instance, self.backup_ids)
+        self.mocked_backed.delete_volumes.assert_called_once_with(
+            membership=self.instance.cloud_project_membership,
+            volume_ids=[self.copied_system_volume_id, self.copied_data_volume_id],
+        )
