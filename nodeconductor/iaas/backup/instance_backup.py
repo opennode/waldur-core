4ff7e24b92cad52d0567205827cb4bb4445c6b6a
Pavel <marchukpavelp@gmail.com>
2015-01-05 16:47:25 +0200

    Rewrite instance backup strategy (nc-283)

--- nodeconductor/iaas/backup/instance_backup.py
+++ nodeconductor/iaas/backup/instance_backup.py
@@ -2,6 +2,7 @@ from django.utils import six
 
 from nodeconductor.backup.models import BackupStrategy
 from nodeconductor.backup.exceptions import BackupStrategyExecutionError
+from nodeconductor.iaas import tasks
 from nodeconductor.iaas.backend import CloudBackendError
 from nodeconductor.iaas.models import Instance
 
@@ -14,32 +15,68 @@ class InstanceBackupStrategy(BackupStrategy):
 
     @classmethod
     def backup(cls, instance):
+        """
+        Copy instance volumes and return new volumes ids and info about instance
+        """
         try:
             backend = cls._get_backend(instance)
-            backup_ids = backend.backup_instance(instance)
-            return ','.join(backup_ids)
+            copied_system_volume_id, copied_data_volume_id = backend.copy_volumes(
+                membership=instance.cloud_project_membership,
+                volume_ids=[instance.system_volume_id, instance.data_volume_id]
+            )
+        except CloudBackendError as e:
+            six.reraise(BackupStrategyExecutionError, e)
+        additional_data = cls._get_instance_addition_data(instance)
+        additional_data['system_volume_id'] = copied_system_volume_id
+        additional_data['data_volume_id'] = copied_data_volume_id
+        return additional_data
+
+    @classmethod
+    def restore(cls, source, additional_data, key, flavor, hostname=None):
+        """
+        Create new instance from backup, key and flavor has to be defined, because old one could be deleted
+        """
+        try:
+            backend = cls._get_backend(source)
+            copied_system_volume_id, copied_data_volume_id = backend.copy_volumes(
+                membership=source.cloud_project_membership,
+                volume_ids=[additional_data.pop('system_volume_id'), additional_data.pop('data_volume_id')]
+            )
         except CloudBackendError as e:
             six.reraise(BackupStrategyExecutionError, e)
 
-    @classmethod
-    def restore(cls, instance, backup_ids):
-        try:
-            if backup_ids:
-                backup_ids = backup_ids.split(',')
-            backend = cls._get_backend(instance)
-            vm = backend.restore_instance(instance, backup_ids)
-            instance.backend_id = vm.id
-            instance.save()
-        except CloudBackendError as e:
-            six.reraise(BackupStrategyExecutionError, e)
+        restored_instance = Instance(cloud_project_membership=source.cloud_project_membership)
+        if hostname is not None:
+            restored_instance.hostname = hostname
+            del additional_data['hostname']
+
+        for k, v in additional_data.iteritems():
+            setattr(restored_instance, k, v)
+
+        restored_instance.cores = flavor.cores
+        restored_instance.ram = flavor.ram
+        restored_instance.system_volume_size = flavor.disk
+
+        restored_instance.key_name = key.name
+        restored_instance.key_fingerprint = key.fingerprint
+
+        restored_instance.save()
+
+        tasks.schedule_provisioning.delay(
+            restored_instance.uuid.hex,
+            backend_flavor_id=flavor.backend_id,
+            system_volume_id=copied_system_volume_id,
+            data_volume_id=copied_data_volume_id
+        )
 
     @classmethod
-    def delete(cls, instance, backup_ids):
+    def delete(cls, source, additional_data):
         try:
-            if backup_ids:
-                backup_ids = backup_ids.split(',')
-            backend = cls._get_backend(instance)
-            backend.delete_instance_backup(instance, backup_ids)
+            backend = cls._get_backend(source)
+            backend.delete_volumes(
+                membership=source.cloud_project_membership,
+                volume_ids=[additional_data['system_volume_id'], additional_data['data_volume_id']]
+            )
         except CloudBackendError as e:
             six.reraise(BackupStrategyExecutionError, e)
 
@@ -47,3 +84,18 @@ class InstanceBackupStrategy(BackupStrategy):
     @classmethod
     def _get_backend(cls, instance):
         return instance.cloud_project_membership.cloud.get_backend()
+
+    @classmethod
+    def _get_instance_addition_data(cls, instance):
+        """
+        Return additional instance information, that have to be stored with backup
+        """
+        return {
+            'hostname': instance.hostname,
+            'template_id': instance.template_id,
+            'external_ips': instance.external_ips,
+            'internal_ips': instance.internal_ips,
+            'agreed_sla': instance.agreed_sla,
+            'system_volume_size': instance.system_volume_size,
+            'data_volume_size': instance.data_volume_size,
+        }
