c4717fbe5734f2642be4a483ad54ead70fd33e08
Ihor Kaharlichenko <ihor@opennodecloud.com>
2015-01-07 15:15:37 +0400

    Force celery reuse Django's logging config
    NC-282

--- nodeconductor/server/celery.py
+++ nodeconductor/server/celery.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
 
+import logging.config
 import os
 
 from celery import Celery
+from celery.signals import setup_logging
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
@@ -14,3 +16,11 @@ app = Celery('nodeconductor')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+# Taken from https://github.com/celery/celery/issues/1867#issuecomment-34867070
+@setup_logging.connect
+def setup_logging(loglevel, logfile, format, colorize, **kwargs):
+    from django.conf import settings
+
+    logging.config.dictConfig(settings.LOGGING)
