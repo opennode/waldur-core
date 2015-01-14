75225c176135b6d17c8f10ea67dabb4b0fc02505
Pavel <marchukpavelp@gmail.com>
2015-01-14 10:32:08 +0200

    Remove field duplication from migrations(nc-301)

--- nodeconductor/iaas/migrations/0009_add_min_ram_and_disk_to_image.py
+++ nodeconductor/iaas/migrations/0009_add_min_ram_and_disk_to_image.py
@@ -24,10 +24,4 @@ class Migration(migrations.Migration):
             field=models.PositiveIntegerField(default=0, help_text='Minimum memory size in MiB'),
             preserve_default=True,
         ),
-        migrations.AlterField(
-            model_name='instance',
-            name='state',
-            field=django_fsm.FSMIntegerField(default=1, help_text='WARNING! Should not be changed manually unless you really know what you are doing.', max_length=1, choices=[(1, 'Provisioning Scheduled'), (2, 'Provisioning'), (3, 'Online'), (4, 'Offline'), (5, 'Starting Scheduled'), (6, 'Starting'), (7, 'Stopping Scheduled'), (8, 'Stopping'), (9, 'Erred'), (10, 'Deletion Scheduled'), (11, 'Deleting'), (13, 'Resizing Scheduled'), (14, 'Resizing'), (15, 'Restarting Scheduled'), (16, 'Restarting')]),
-            preserve_default=True,
-        ),
     ]
