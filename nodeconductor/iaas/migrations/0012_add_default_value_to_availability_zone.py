7d07408b6eaff93708c3565d45a85aca449139a5
Ilja Livenson <ilja@opennodecloud.com>
2015-01-22 17:30:58 +0400

    Fix migrations for the availability zone
    - avoid IntegrityError for migrations in sqlite

--- nodeconductor/iaas/migrations/0012_add_default_value_to_availability_zone.py
+++ nodeconductor/iaas/migrations/0012_add_default_value_to_availability_zone.py
@@ -7,14 +7,14 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('iaas', '0010_auto_20150118_1834'),
+        ('iaas', '0011_cloudprojectmembership_availability_zone'),
     ]
 
     operations = [
-        migrations.AddField(
+        migrations.AlterField(
             model_name='cloudprojectmembership',
             name='availability_zone',
-            field=models.CharField(default="", help_text='Optional availability group. Will be used for all instances provisioned in this tenant', max_length=100, blank=True),
+            field=models.CharField(default='', help_text='Optional availability group. Will be used for all instances provisioned in this tenant', max_length=100, blank=True),
             preserve_default=True,
         ),
     ]
