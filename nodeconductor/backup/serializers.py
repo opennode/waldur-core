f0c3c5db80bab426069c1fcd79fa9b805f0b91f4
Pavel <marchukpavelp@gmail.com>
2015-01-27 18:43:21 +0200

    GenericRelatedField was moved to core (nc-263)

--- nodeconductor/backup/serializers.py
+++ nodeconductor/backup/serializers.py
@@ -1,91 +1,12 @@
-from django.core.exceptions import ValidationError
-from django.core.urlresolvers import reverse, resolve, Resolver404
-from nodeconductor.structure.serializers import fix_non_nullable_attrs
-
 from rest_framework import serializers
-from rest_framework.relations import RelatedField
 
 from nodeconductor.backup import models, utils
-
-
-class RelatedBackupField(RelatedField):
-    """
-    A custom field to use for the `tagged_object` generic relationship.
-    """
-    read_only = False
-    _default_view_name = '%(model_name)s-detail'
-    lookup_field = 'uuid'
-
-    def _get_url(self, obj):
-        """
-        Gets object url
-        """
-        format_kwargs = {
-            'app_label': obj._meta.app_label,
-            'model_name': obj._meta.object_name.lower()
-        }
-        return self._default_view_name % format_kwargs
-
-    def to_native(self, obj):
-        """
-        Serializes any object to his url representation
-        """
-        kwargs = {self.lookup_field: getattr(obj, self.lookup_field)}
-        try:
-            request = self.context['request']
-        except AttributeError:
-            raise AttributeError('RelatedBackupField have to be initialized with `request` in context')
-        return request.build_absolute_uri(reverse(self._get_url(obj), kwargs=kwargs))
-
-    def _format_url(self, url):
-        """
-        Removes domain and protocol from url
-        """
-        if url.startswith('http'):
-            return '/' + url.split('/', 3)[-1]
-        return url
-
-    def _get_model_from_resolve_match(self, match):
-        queryset = match.func.cls.queryset
-        if queryset is not None:
-            return queryset.model
-        else:
-            return match.func.cls.model
-
-    def from_native(self, data):
-        """
-        Restores model instance from its url
-        """
-        try:
-            url = self._format_url(data)
-            match = resolve(url)
-            model = self._get_model_from_resolve_match(match)
-            obj = model.objects.get(**match.kwargs)
-        except (Resolver404, AttributeError):
-            raise ValidationError("Can`t restore object from url: %s" % data)
-        if not utils.has_object_backup_strategy(obj):
-            raise ValidationError('%s object is unbackupable' % str(obj))
-        return obj
-
-    # this method tries to initialize queryset based on field.rel.to._default_manager
-    # but generic field does not have default manager
-    def initialize(self, parent, field_name):
-        super(RelatedField, self).initialize(parent, field_name)
-
-        supported_models = utils.get_backup_strategies().values()
-        if len(supported_models) < 1:
-            # XXX: setting queryset to None is not the best idea. Better empty set models are welcome
-            self.queryset = None
-            return
-
-        # XXX ideally this queryset has to return all available for generic key instances
-        # Now we just take first backupable model and return all its instances
-        model = supported_models[0].get_model()
-        self.queryset = model.objects.all()
+from nodeconductor.core.serializers import GenericRelatedField
+from nodeconductor.structure.serializers import fix_non_nullable_attrs
 
 
 class BackupScheduleSerializer(serializers.HyperlinkedModelSerializer):
-    backup_source = RelatedBackupField()
+    backup_source = GenericRelatedField(related_models=utils.get_backupable_models())
 
     class Meta(object):
         model = models.BackupSchedule
@@ -100,7 +21,7 @@ class BackupScheduleSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class BackupSerializer(serializers.HyperlinkedModelSerializer):
-    backup_source = RelatedBackupField()
+    backup_source = GenericRelatedField(related_models=utils.get_backupable_models())
     state = serializers.ChoiceField(choices=models.Backup.STATE_CHOICES, source='get_state_display', read_only=True)
 
     class Meta(object):
--- nodeconductor/core/serializers.py
+++ nodeconductor/core/serializers.py
@@ -2,8 +2,11 @@ import base64
 
 from django.contrib.auth import authenticate
 from django.core import validators
+from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse, resolve, Resolver404
 from rest_framework import serializers
 from rest_framework.fields import Field
+from rest_framework.relations import RelatedField
 
 from nodeconductor.core.signals import pre_serializer_fields
 from nodeconductor.structure.filters import filter_queryset_for_user
@@ -225,3 +228,81 @@ class CollectedFieldsMixin(object):
         fields = super(CollectedFieldsMixin, self).get_fields()
         pre_serializer_fields.send(sender=self.__class__, fields=fields)
         return fields
+
+
+class GenericRelatedField(RelatedField):
+    """
+    A custom field to use for the `tagged_object` generic relationship.
+    """
+    read_only = False
+    _default_view_name = '%(model_name)s-detail'
+    lookup_field = 'uuid'
+
+    def __init__(self, related_models=[], **kwargs):
+        super(GenericRelatedField, self).__init__(**kwargs)
+        self.related_models = related_models
+
+    def _get_url(self, obj):
+        """
+        Gets object url
+        """
+        format_kwargs = {
+            'app_label': obj._meta.app_label,
+            'model_name': obj._meta.object_name.lower()
+        }
+        return self._default_view_name % format_kwargs
+
+    def to_native(self, obj):
+        """
+        Serializes any object to his url representation
+        """
+        kwargs = {self.lookup_field: getattr(obj, self.lookup_field)}
+        try:
+            request = self.context['request']
+        except AttributeError:
+            raise AttributeError('RelatedBackupField have to be initialized with `request` in context')
+        return request.build_absolute_uri(reverse(self._get_url(obj), kwargs=kwargs))
+
+    def _format_url(self, url):
+        """
+        Removes domain and protocol from url
+        """
+        if url.startswith('http'):
+            return '/' + url.split('/', 3)[-1]
+        return url
+
+    def _get_model_from_resolve_match(self, match):
+        queryset = match.func.cls.queryset
+        if queryset is not None:
+            return queryset.model
+        else:
+            return match.func.cls.model
+
+    def from_native(self, data):
+        """
+        Restores model instance from its url
+        """
+        try:
+            url = self._format_url(data)
+            match = resolve(url)
+            model = self._get_model_from_resolve_match(match)
+            obj = model.objects.get(**match.kwargs)
+        except (Resolver404, AttributeError):
+            raise ValidationError("Can`t restore object from url: %s" % data)
+        if model not in self.related_models:
+            raise ValidationError('%s object does not support such relationship' % str(obj))
+        return obj
+
+    # this method tries to initialize queryset based on field.rel.to._default_manager
+    # but generic field does not have default manager
+    def initialize(self, parent, field_name):
+        super(RelatedField, self).initialize(parent, field_name)
+
+        if len(self.related_models) < 1:
+            self.queryset = set()
+            return
+
+        # XXX ideally this queryset has to return all available for generic key instances
+        # Now we just take first backupable model and return all its instances
+        model = self.related_models[0]
+        self.queryset = model.objects.all()
