70eca2c34289f4acb2cea62237cb42e225d4296d
Pavel <marchukpavelp@gmail.com>
2015-01-30 13:39:45 +0200

    No pagination with quotas (nc-263)

--- nodeconductor/quotas/views.py
+++ nodeconductor/quotas/views.py
@@ -6,9 +6,11 @@ from nodeconductor.quotas import models, serializers
 
 class QuotaViewSet(core_viewsets.UpdateModelViewSet):
 
+    queryset = models.Quota.objects.all()
     serializer_class = serializers.QuotaSerializer
     lookup_field = 'uuid'
     permission_classes = (rf_permissions.IsAuthenticated,)
+    paginate_by = None
 
     def get_queryset(self):
         return models.Quota.objects.filtered_for_user(self.request.user)
