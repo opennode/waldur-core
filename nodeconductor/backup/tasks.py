81fbab0045a0c8bb4acf708eaeee523d2d7e9d89
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-02-04 11:49:50 +0200

    Fix messages in event logs
    NC-328

--- nodeconductor/backup/tasks.py
+++ nodeconductor/backup/tasks.py
@@ -23,8 +23,8 @@ def process_backup_task(backup_uuid):
                 backup.confirm_backup()
             except exceptions.BackupStrategyExecutionError:
                 logger.exception('Failed to perform backup for backup source: %s', backup.backup_source)
-                event_logger.exception('Failed to create backup for %s', backup.backup_source,
-                                       extra={'event_type': 'iaas_backup_create_failed'})
+                event_logger.error('Backup creation for %s failed', backup.backup_source,
+                                   extra={'event_type': 'iaas_backup_create_failed'})
                 backup.erred()
             else:
                 logger.info('Successfully performed backup for backup source: %s', backup.backup_source)
@@ -48,8 +48,8 @@ def restoration_task(backup_uuid, instance_uuid, user_raw_input):
                 backup.confirm_restoration()
             except exceptions.BackupStrategyExecutionError:
                 logger.exception('Failed to restore backup for backup source: %s', backup.backup_source)
-                event_logger.info('Failed to restore backup for %s', backup.backup_source,
-                                  extra={'event_type': 'iaas_backup_restore_failed'})
+                event_logger.error('Backup restoration for %s failed', backup.backup_source,
+                                   extra={'event_type': 'iaas_backup_restore_failed'})
                 backup.erred()
             else:
                 logger.info('Successfully restored backup for backup source: %s', backup.backup_source)
@@ -74,8 +74,8 @@ def deletion_task(backup_uuid):
                 backup.confirm_deletion()
             except exceptions.BackupStrategyExecutionError:
                 logger.exception('Failed to delete backup for backup source: %s', backup.backup_source)
-                event_logger.info('Failed to delete backup for %s', backup.backup_source,
-                                  extra={'event_type': 'iaas_backup_delete_failed'})
+                event_logger.error('Backup deletion for %s failed', backup.backup_source,
+                                   extra={'event_type': 'iaas_backup_delete_failed'})
                 backup.erred()
             else:
                 logger.info('Successfully deleted backup for backup source: %s', backup.backup_source)
