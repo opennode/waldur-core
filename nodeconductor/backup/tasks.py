21ce49a03c1697e3764c088f73fd41422af8886b
Roman Kosenko <Roman.Kosenko@gmail.com>
2015-02-04 13:10:06 +0000

    Deactivate schedule if backup fails
    - NC-355

--- nodeconductor/backup/tasks.py
+++ nodeconductor/backup/tasks.py
@@ -4,9 +4,11 @@ from celery import shared_task
 from django.utils import timezone
 
 from nodeconductor.backup import models, exceptions
+from nodeconductor.core.log import EventLoggerAdapter
 
 
 logger = logging.getLogger(__name__)
+event_logger = EventLoggerAdapter(logger)
 
 
 @shared_task
@@ -20,6 +22,15 @@ def process_backup_task(backup_uuid):
                 backup.metadata = backup.get_strategy().backup(backup.backup_source)
                 backup.confirm_backup()
             except exceptions.BackupStrategyExecutionError:
+                schedule = backup.backup_schedule
+                if schedule:
+                    event_logger.info(
+                        'Backup schedule deactivated for backup source: %s', backup.backup_source,
+                        extra={'backup': last_backup, 'event_type': 'iaas_backup_schedule_deactivated'}
+                    )
+                    schedule.is_active = False
+                    schedule.save(update_fields=['is_active'])
+
                 logger.exception('Failed to perform backup for backup source: %s', backup.backup_source)
                 backup.erred()
             else:
@@ -67,7 +78,7 @@ def deletion_task(backup_uuid):
             else:
                 logger.info('Successfully deleted backup for backup source: %s', backup.backup_source)
         else:
-            logger.exception('Restoration task was called for backup with no source. Backup uuid: %s', backup_uuid)
+            logger.exception('Deletion task was called for backup with no source. Backup uuid: %s', backup_uuid)
     except models.Backup.DoesNotExist:
         logger.exception('Deletion task was called for backed with uuid %s which does not exist', backup_uuid)
 
