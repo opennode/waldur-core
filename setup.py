d902cc97ba73c0ac84c3bd9903eb27ba2d94e23c
Ilja Livenson <ilja@opennodecloud.com>
2015-02-20 23:17:50 +0400

    Starting 0.39 release cycle

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ install_requires = [
 
 setup(
     name='nodeconductor',
-    version='0.38.0',
+    version='0.39.0.dev0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/nodeconductor',
