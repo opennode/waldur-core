8adbf9de3cb5eb288db59619bae42f0141119bd1
Ihor Kaharlichenko <ihor@opennodecloud.com>
2015-02-26 15:20:49 +0400

    Bump version to 0.38.3

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ install_requires = [
 
 setup(
     name='nodeconductor',
-    version='0.38.2',
+    version='0.38.3',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/nodeconductor',
