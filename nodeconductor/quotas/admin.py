b474c8e9c7eb0fcc758b9bcf049cb8ecb41e9c7a
Pavel <marchukpavelp@gmail.com>
2015-03-17 17:44:38 +0200

    Implement admin model for quotas (nc-417)

---
+++ nodeconductor/quotas/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.contrib.contenttypes import models as ct_models
+
+from nodeconductor.quotas import models, utils
+
+
+class QuotaScopeClassListFilter(admin.SimpleListFilter):
+    # Human-readable title
+    title = 'Scope class'
+
+    # Parameter for the filter that will be used in the URL query
+    parameter_name = 'scope_class'
+
+    def lookups(self, request, model_admin):
+        models = utils.get_models_with_quotas()
+        return [(ct_models.ContentType.objects.get_for_model(m).id, m.__name__) for m in models]
+
+    def queryset(self, request, queryset):
+        content_type_id = self.value()
+        if content_type_id:
+            return queryset.filter(content_type_id=content_type_id)
+        return queryset
+
+
+class QuotaAdmin(admin.ModelAdmin):
+    list_display = ['scope', 'name', 'limit', 'usage']
+    list_filter = ['name', QuotaScopeClassListFilter]
+
+
+admin.site.register(models.Quota, QuotaAdmin)
