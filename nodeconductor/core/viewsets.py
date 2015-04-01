870f07bbcc491f819ca4279f8f952eb6193ee5b1
Ihor Kaharlichenko <ihor@opennodecloud.com>
2015-04-01 15:58:58 +0400

    Drop custom DestroyModelMixin
    NC-439

--- nodeconductor/core/viewsets.py
+++ nodeconductor/core/viewsets.py
@@ -2,28 +2,28 @@ from __future__ import unicode_literals
 
 import warnings
 
-from rest_framework import mixins as rf_mixins
-from rest_framework import viewsets as rf_viewsets
-
-from nodeconductor.core import mixins
+from rest_framework import mixins
+from rest_framework import viewsets
 
 
-class ModelViewSet(rf_mixins.CreateModelMixin,
-                   rf_mixins.RetrieveModelMixin,
-                   rf_mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   rf_mixins.ListModelMixin,
-                   rf_viewsets.GenericViewSet):
+class ModelViewSet(viewsets.ModelViewSet):
     """
     A viewset that provides default `create()`, `retrieve()`, `update()`,
     `partial_update()`, `destroy()` and `list()` actions.
     """
-    pass
+
+    def __init__(self, **kwargs):
+        warnings.warn(
+            "nodeconductor.core.viewsets.ModelViewSet is deprecated. "
+            "Use stock rest_framework.viewsets.ModelViewSet instead.",
+            DeprecationWarning,
+        )
+        super(ModelViewSet, self).__init__(**kwargs)
 
 
-class ReadOnlyModelViewSet(rf_mixins.RetrieveModelMixin,
-                           rf_mixins.ListModelMixin,
-                           rf_viewsets.GenericViewSet):
+class ReadOnlyModelViewSet(mixins.RetrieveModelMixin,
+                           mixins.ListModelMixin,
+                           viewsets.GenericViewSet):
     """
     A viewset that provides default `list()` and `retrieve()` actions.
     """
@@ -35,20 +35,3 @@ class ReadOnlyModelViewSet(rf_mixins.RetrieveModelMixin,
         )
 
         super(ReadOnlyModelViewSet, self).__init__(*args, **kwargs)
-
-
-class CreateModelViewSet(rf_mixins.CreateModelMixin,
-                         rf_mixins.RetrieveModelMixin,
-                         rf_mixins.ListModelMixin,
-                         rf_viewsets.GenericViewSet):
-    """
-    A viewset that provides default `create()`, `list()` and `retrieve()` actions.
-    """
-    def __init__(self, *args, **kwargs):
-        warnings.warn(
-            "nodeconductor.core.viewsets.CreateModelViewSet is deprecated. "
-            "Use combination of stock rest_framework.mixins.* and GenericViewSet instead.",
-            DeprecationWarning,
-        )
-
-        super(CreateModelViewSet, self).__init__(*args, **kwargs)
