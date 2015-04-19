eac2f296e855f92d040321edee943ad5f8a8fb39
Pavel <marchukpavelp@gmail.com>
2015-04-19 12:56:20 +0400

    Add filtering to view (nc-463)

--- nodeconductor/events/views.py
+++ nodeconductor/events/views.py
@@ -7,7 +7,10 @@ class EventListView(generics.GenericAPIView):
 
     def list(self, request, *args, **kwargs):
         order_by = request.GET.get('o', '-@timestamp')
-        elasticsearch_list = elasticsearch_client.ElasticsearchResultList(user=request.user, sort=order_by)
+        event_types = request.GET.getlist('event_type')
+        search_text = request.GET.get('search_text')
+        elasticsearch_list = elasticsearch_client.ElasticsearchResultList(
+            user=request.user, sort=order_by, event_types=event_types, search_text=search_text)
 
         page = self.paginate_queryset(elasticsearch_list)
         if page is not None:
