b34ff97bec4bc2cca1247d40828c0f95a03b0e47
Pavel <marchukpavelp@gmail.com>
2015-04-20 16:22:59 +0400

    Implement tests for events (nc-468)

--- nodeconductor/events/tests/factories.py
+++ nodeconductor/events/tests/factories.py
@@ -1,3 +1,7 @@
+from __future__ import unicode_literals
+
+from django.core.urlresolvers import reverse
+
 from nodeconductor.events import elasticsearch_dummy_client
 
 
@@ -9,27 +13,28 @@ class EventFactory(object):
 
     def create(self, **kwargs):
         self.fields = {
-            u'@timestamp': u'2015-04-19T16:25:45.376+04:00',
-            u'@version': 1,
-            u'cloud_account_name': u'test_cloud_account_name',
-            u'cloud_account_uuid': u'test_cloud_account_uuid',
-            u'customer_abbreviation': u'TCAN',
-            u'customer_contact_details': u'test details',
-            u'customer_name': u'Test cusomter',
-            u'customer_uuid': u'test_customer_uuid',
-            u'event_type': u'test_event_type',
-            u'host': u'example.com',
-            u'importance': u'high',
-            u'importance_code': 30,
-            u'levelname': u'WARNING',
-            u'logger': u'nodeconductor.test',
-            u'message': u'Test message',
-            u'project_group_name': u'test_group_name',
-            u'project_group_uuid': u'test_group_uuid',
-            u'project_name': u'test_project',
-            u'project_uuid': u'test_project_uuid',
-            u'tags': [u'_jsonparsefailure'],
-            u'type': u'gcloud-event',
+            '@timestamp': '2015-04-19T16:25:45.376+04:00',
+            '@version': 1,
+            'cloud_account_name': 'test_cloud_account_name',
+            'cloud_account_uuid': 'test_cloud_account_uuid',
+            'customer_abbreviation': 'TCAN',
+            'customer_contact_details': 'test details',
+            'customer_name': 'Test cusomter',
+            'customer_uuid': 'test_customer_uuid',
+            'event_type': 'test_event_type',
+            'host': 'example.com',
+            'importance': 'high',
+            'importance_code': 30,
+            'levelname': 'WARNING',
+            'logger': 'nodeconductor.test',
+            'message': 'Test message',
+            'project_group_name': 'test_group_name',
+            'project_group_uuid': 'test_group_uuid',
+            'project_name': 'test_project',
+            'project_uuid': 'test_project_uuid',
+            'tags': ['_jsonparsefailure'],
+            'type': 'gcloud-event',
+            'user_uuid': 'test_user_uuid',
         }
         for key, value in self.fields.items():
             if key in kwargs:
@@ -37,3 +42,7 @@ class EventFactory(object):
 
     def save(self):
         elasticsearch_dummy_client.ElasticsearchDummyClient.DUMMY_EVENTS.append(self.fields)
+
+    @classmethod
+    def get_list_url(cls):
+        return 'http://testserver' + reverse('event-list')
