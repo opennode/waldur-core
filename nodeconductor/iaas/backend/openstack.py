5412f8b7c4f288c964b129feecf632136f26d1cf
Ilja Livenson <ilja@opennodecloud.com>
2015-05-04 09:57:59 +0300

    Fix calling of the router creation function

--- nodeconductor/iaas/backend/openstack.py
+++ nodeconductor/iaas/backend/openstack.py
@@ -1855,7 +1855,7 @@ class OpenStackBackend(OpenStackClient):
 
                 network_name = response['network']['name']
                 subnet_id = response['network']['subnets'][0]
-                self.get_or_create_router(neutron, network_name, subnet_id)
+                self.get_or_create_router(neutron, network_name, subnet_id, membership.tenant_id)
 
             return membership.internal_network_id
 
