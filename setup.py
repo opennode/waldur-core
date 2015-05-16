770a2ca6fe685b73885d5002a6439c81f3883c72
Ilja Livenson <ilja@opennodecloud.com>
2015-05-16 17:26:54 +0300

    Starting a new release.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ install_requires = [
 
 setup(
     name='nodeconductor',
-    version='0.48.0',
+    version='0.49.0.dev0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/nodeconductor',
