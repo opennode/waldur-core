83e9cf4f6385daeec2de6efb724d946c4f502631
Ilja Livenson <ilja@opennodecloud.com>
2015-05-29 14:07:45 +0400

    Remove default value of a FK in migration
    - NC-516
    - Fails migration on MySQL

--- nodeconductor/structure/migrations/0009_update_service_models.py
+++ nodeconductor/structure/migrations/0009_update_service_models.py
@@ -44,7 +44,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='service',
             name='settings',
-            field=models.ForeignKey(related_name='+', default=0, to='structure.ServiceSettings'),
+            field=models.ForeignKey(related_name='+', to='structure.ServiceSettings'),
             preserve_default=False,
         ),
     ]
