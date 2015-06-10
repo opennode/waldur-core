e789b87daacfd7b9bf4c322d9ad9e45a128fe5fc
Roman Kosenko <Roman.Kosenko@gmail.com>
2015-06-10 09:46:41 +0000

    Resource unlink action
    - SAAS-443

--- nodeconductor/structure/views.py
+++ nodeconductor/structure/views.py
@@ -1080,6 +1080,12 @@ class BaseResourceViewSet(core_mixins.UserContextMixin, viewsets.ModelViewSet):
             resource.delete()
 
     @detail_route(methods=['post'])
+    def unlink(self, request, uuid=None):
+        resource = self.get_object()
+        resource.delete()
+        return Response({'status': 'resource unlinked'}, status=status.HTTP_200_OK)
+
+    @detail_route(methods=['post'])
     def start(self, request, uuid=None):
         resource = self.get_object()
         if resource.state != resource.States.OFFLINE:
