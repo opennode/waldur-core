aa7fb02e9d04e6cb1b7fd7c622c640e58734c014
Pavel <marchukpavelp@gmail.com>
2015-06-15 12:57:58 +0400

    Alerts filtering (nc-547)

--- nodeconductor/logging/serializers.py
+++ nodeconductor/logging/serializers.py
@@ -1,3 +1,5 @@
+import re
+
 from rest_framework import serializers
 
 from nodeconductor.core.serializers import GenericRelatedField
@@ -21,3 +23,20 @@ class AlertSerializer(serializers.HyperlinkedModelSerializer):
         extra_kwargs = {
             'url': {'lookup_field': 'uuid'},
         }
+
+
+class ScopeSerializer(serializers.Serializer):
+    scope = GenericRelatedField(related_models=utils.get_loggable_models())
+
+
+def _convert(name):
+    """ Converts CamelCase to underscore """
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+class ScopeTypeSerializer(serializers.Serializer):
+    scope_type = MappedChoiceField(
+        choices=[(_convert(m.__name__), m.__name__) for m in utils.get_loggable_models()],
+        choice_mappings={_convert(m.__name__): m for m in utils.get_loggable_models()},
+    )
