93ec5f4c2ff64f1c05c88396d2339401e87963ab
Victor Mireyev <victor@opennodecloud.com>
2015-06-21 00:32:22 +0300

    Fix migration (SAAS-436)

--- nodeconductor/structure/migrations/0012_customer_image.py
+++ nodeconductor/structure/migrations/0012_customer_image.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import nodeconductor.structure.images
 
 
 class Migration(migrations.Migration):
@@ -14,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='customer',
             name='image',
-            field=models.ImageField(null=True, blank=True),
+            field=models.ImageField(null=True, upload_to=nodeconductor.structure.images.get_upload_path, blank=True),
             preserve_default=True,
         ),
     ]
