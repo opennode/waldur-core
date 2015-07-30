b1873a7718d829e706e5cd1627a6ec4a73d55ddb
Pavel <marchukpavelp@gmail.com>
2015-07-30 09:47:19 +0400

    Add tests for price list endpoint (nc-690)

--- nodeconductor/cost_tracking/tests/factories.py
+++ nodeconductor/cost_tracking/tests/factories.py
@@ -3,6 +3,7 @@ import factory
 from django.core.urlresolvers import reverse
 
 from nodeconductor.cost_tracking import models
+from nodeconductor.oracle.tests import factories as oracle_factories
 from nodeconductor.structure.tests import factories as structure_factories
 
 
@@ -25,3 +26,40 @@ class PriceEstimateFactory(factory.DjangoModelFactory):
             price_estimate = PriceEstimateFactory()
         url = 'http://testserver' + reverse('priceestimate-detail', kwargs={'uuid': price_estimate.uuid})
         return url if action is None else url + action + '/'
+
+
+class PriceListFactory(factory.DjangoModelFactory):
+    class Meta(object):
+        model = models.PriceList
+
+    service = factory.SubFactory(oracle_factories.OracleServiceFactory)
+
+    @factory.post_generation
+    def items(self, create, extracted, **kwargs):
+        if create:
+            if extracted:
+                for item in extracted:
+                    self.items.create(**item)
+            else:
+                for _ in range(2):
+                    PriceListItemFactory(price_list=self)
+
+    @classmethod
+    def get_list_url(self):
+        return 'http://testserver' + reverse('pricelist-list')
+
+    @classmethod
+    def get_url(self, price_list, action=None):
+        if price_list is None:
+            price_list = PriceListFactory()
+        url = 'http://testserver' + reverse('pricelist-detail', kwargs={'uuid': price_list.uuid})
+        return url if action is None else url + action + '/'
+
+
+class PriceListItemFactory(factory.DjangoModelFactory):
+    class Meta(object):
+        model = models.PriceListItem
+
+    price_list = factory.SubFactory(PriceListFactory)
+    name = factory.Iterator(['cpu', 'memory', 'storage'])
+    value = factory.Iterator([10, 100, 1000, 10000, 1313, 13])
