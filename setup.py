6198251e194c6402032462f034c2dafb50041dc5
Jenkins <jenkins@opennodecloud.com>
2015-08-09 18:17:04 +0000

    Preparing new release: 0.72.0

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ install_requires = [
 
 setup(
     name='nodeconductor',
-    version='0.71.0',
+    version='0.72.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/nodeconductor',
