20f883bbc17d4cde5c0a1d3cdc760c4dcf4f064a
Victor Mireyev <victor@opennodecloud.com>
2015-08-10 16:30:57 +0300

    Implement recurring payments in PayPal backend wrapper (SAAS-445)

--- nodeconductor/billing/backend/paypal.py
+++ nodeconductor/billing/backend/paypal.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.utils import six
 import paypalrestsdk as paypal
 
@@ -58,5 +60,94 @@ class PaypalBackend(object):
                 return True
             else:
                 raise BillingBackendError(payment.error)
+        except paypal.exceptions.ConnectionError as e:
+            six.reraise(BillingBackendError, e)
+
+    def create_plan(self, amount, name, description, return_url, cancel_url):
+        """
+        Create and activate montlhy billing plan using PayPal Rest API.
+        On success returns plan_id
+        """
+        plan = paypal.BillingPlan({
+            'name': name,
+            'description': description,
+            'type': 'INFINITE',
+            'payment_definitions': [{
+                'name': 'Monthly payment for {}'.format(name),
+                'type': 'REGULAR',
+                'frequency_interval': 1,
+                'frequency': 'MONTH',
+                'cycles': 0,
+                'amount': {
+                    'currency': self.currency_name,
+                    'value': str(amount)
+                }
+            }],
+            'merchant_preferences': {
+                'return_url': return_url,
+                'cancel_url': cancel_url,
+                'auto_bill_amount': 'YES',
+            }
+        })
+
+        try:
+            if plan.create() and plan.activate():
+                return plan.id
+            else:
+                raise BillingBackendError(plan.error)
+        except paypal.exceptions.ConnectionError as e:
+            six.reraise(BillingBackendError, e)
+
+    def create_agreement(self, plan_id, name):
+        """
+        Create billing agreement. On success returns approval_url
+        """
+        start_date = datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+        formatted_date = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+        agreement = paypal.BillingAgreement({
+            'name': name,
+            'description': 'Agreement for {}'.format(name),
+            'start_date': formatted_date,
+            'payer': {'payment_method': 'paypal'},
+            'plan': {'id': plan_id}
+        })
+        try:
+            if agreement.create():
+                for link in agreement.links:
+                    if link.rel == 'approval_url':
+                        approval_url = link.href
+                        return approval_url
+            else:
+                raise BillingBackendError(agreement.error)
+        except paypal.exceptions.ConnectionError as e:
+            six.reraise(BillingBackendError, e)
+
+    def execute_agreement(self, payment_token):
+        """
+        Agreement should be executed if user has approved it.
+        On success returns agreement id
+        """
+        try:
+            agreement = paypal.BillingAgreement.execute(payment_token)
+            return agreement.id
+        except paypal.exceptions.ConnectionError as e:
+            six.reraise(BillingBackendError, e)
+
+    def cancel_agreement(self, agreement_id):
+        try:
+            agreement = BillingAgreement.find(agreement_id)
+            if agreement.cancel({'note': 'Canceling the agreement by application'}):
+                return True
+            else:
+                raise BillingBackendError(agreement.error)
+        except paypal.exceptions.ConnectionError as e:
+            six.reraise(BillingBackendError, e)
+
+    def get_agreement_transactions(self, agreement_id, start_date, end_date):
+        try:
+            agreement = paypal.BillingAgreement.find(agreement_id)
+            formatted_start_date = start_date.strftime('%Y-%m-%d')
+            formatted_end_date = end_date.strftime('%Y-%m-%d')
+            return agreement.search_transactions(start_date, end_date)
         except paypal.exceptions.ConnectionError as e:
             six.reraise(BillingBackendError, e)
