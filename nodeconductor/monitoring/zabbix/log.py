65572545dc39ff26b55ef51b47ec58f56965e361
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-09-09 12:47:08 +0300

    Add comment about skipping JSON-RPC log
    NC-759

--- nodeconductor/monitoring/zabbix/log.py
+++ nodeconductor/monitoring/zabbix/log.py
@@ -3,6 +3,7 @@ from logging import Filter
 
 class ZabbixLogsFilter(Filter):
     def filter(self, record):
+        # Mute useless Zabbix log concerning JSON-RPC server endpoint.
         if record.getMessage().startswith('JSON-RPC Server Endpoint'):
             return False
 
