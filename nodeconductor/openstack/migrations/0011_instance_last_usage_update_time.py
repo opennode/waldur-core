2140c2c1cf7296c97649c53b7c3f4adfaebb8d0e
Pavel <marchukpavelp@gmail.com>
2015-09-21 14:56:56 +0400

    Correct migrations
     - nc-802

--- nodeconductor/openstack/migrations/0011_instance_last_usage_update_time.py
+++ nodeconductor/openstack/migrations/0011_instance_last_usage_update_time.py
@@ -9,14 +9,14 @@ from django.utils.timezone import utc
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('iaas', '0047_refactor_application_type_field'),
+        ('openstack', '0010_spl_unique_together_constraint'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='instance',
             name='last_usage_update_time',
-            field=models.DateTimeField(default=datetime.datetime(2015, 9, 21, 7, 48, 17, 947642, tzinfo=utc), auto_now_add=True),
+            field=models.DateTimeField(default=datetime.datetime(2015, 9, 21, 10, 33, 39, 68094, tzinfo=utc), auto_now_add=True),
             preserve_default=False,
         ),
     ]
