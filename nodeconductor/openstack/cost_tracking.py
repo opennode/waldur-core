9668580633a1a8baaa59030e5a52d2478222cbd2
Pavel <marchukpavelp@gmail.com>
2015-11-10 15:12:55 +0400

    Add cost tracking file to openstack
     - saas-951

---
+++ nodeconductor/openstack/cost_tracking.py
@@ -0,0 +1,10 @@
+from . import models
+from nodeconductor.cost_tracking import CostTrackingBackend
+
+
+class OpenStackCostTrackingBackend(CostTrackingBackend):
+
+    @classmethod
+    def get_monthly_cost_estimate(cls, resource):
+        backend = resource.get_backend()
+        return backend.get_monthly_cost_estimate()
