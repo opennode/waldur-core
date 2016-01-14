e3e4aba2713044ab2c8844387a507515346d14a0
Pavel <marchukpavelp@gmail.com>
2016-01-14 11:26:29 +0400

    Correct iaas instance manager
     - itacloud-5835-2

--- nodeconductor/iaas/managers.py
+++ nodeconductor/iaas/managers.py
@@ -11,35 +11,12 @@ class InstanceQueryset(StructureQueryset):
         new_keys = [key.replace('service', 'cloud') for key in new_keys]
         return super(InstanceQueryset, self).order_by(*new_keys)
 
-    def exclude(self, *args, **kwargs):
-        return super(InstanceQueryset, self).exclude(
-            *[self._patch_query_argument(a) for a in args],
-            **self._filter_by_custom_fields(**kwargs))
-
-    def filter(self, *args, **kwargs):
-        return super(InstanceQueryset, self).filter(
-            *[self._patch_query_argument(a) for a in args],
-            **self._filter_by_custom_fields(**kwargs))
-
-    def _patch_query_argument(self, arg):
-        # patch Q() objects if passed and add support of custom fields
-        if isinstance(arg, models.Q):
-            children = []
-            for opt in arg.children:
-                if isinstance(opt, models.Q):
-                    children.append(self._patch_query_argument(opt))
-                else:
-                    args = self._filter_by_custom_fields(**dict([opt]))
-                    children.append(tuple(args.items())[0])
-            arg.children = children
-        return arg
-
     def _filter_by_custom_fields(self, **kwargs):
         # replace filter fields to enable filtering by spl and service.
         for key in kwargs:
             new_key = key.replace('service_project_link', 'cloud_project_membership')
             new_key = new_key.replace('service', 'cloud')
             kwargs[new_key] = kwargs.pop(key)
-        return kwargs
+        return super(InstanceQueryset, self)._filter_by_custom_fields(**kwargs)
 
 InstanceManager = models.Manager.from_queryset(InstanceQueryset)
