03a2aa8f0a29062b360919a61992b5edabe83674
Roman Kosenko <Roman.Kosenko@gmail.com>
2016-01-20 09:20:50 +0000

    Fix default args

--- nodeconductor/openstack/tasks/network.py
+++ nodeconductor/openstack/tasks/network.py
@@ -9,9 +9,12 @@ logger = logging.getLogger(__name__)
 
 
 @shared_task(name='nodeconductor.openstack.sync_external_network')
-def sync_external_network(service_project_link_str, action, data=()):
+def sync_external_network(service_project_link_str, action, data=None):
     service_project_link = next(OpenStackServiceProjectLink.from_string(service_project_link_str))
     backend = service_project_link.get_backend()
+
+    if not data:
+        data = {}
 
     try:
         func = getattr(backend, '%s_external_network' % action)
