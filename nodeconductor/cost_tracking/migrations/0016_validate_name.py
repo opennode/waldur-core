ebf5c86d73ab3de32ce9a1d6f500d3966ce12bd7
Victor Mireyev <victor@opennodecloud.com>
2016-01-27 12:31:54 +0300

    Add missing migration for name validation (NC-1119)

---
+++ nodeconductor/cost_tracking/migrations/0016_validate_name.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import nodeconductor.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cost_tracking', '0015_defaultpricelistitem_metadata'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='applicationtype',
+            name='name',
+            field=models.CharField(max_length=150, verbose_name='name', validators=[nodeconductor.core.validators.validate_name]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='defaultpricelistitem',
+            name='name',
+            field=models.CharField(max_length=150, verbose_name='name', validators=[nodeconductor.core.validators.validate_name]),
+            preserve_default=True,
+        ),
+    ]
