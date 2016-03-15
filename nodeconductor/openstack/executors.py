ff3912e8f15b1783d14679f075d5a9c2987a8b26
Pavel Marchuk <marchukpavelp@gmail.com>
2016-03-15 10:45:41 +0400

    Move executors to separate file.
     - nc-1194

--- nodeconductor/openstack/executors.py
+++ nodeconductor/openstack/executors.py
@@ -1,32 +1,22 @@
-from nodeconductor.structure.tasks import DeleteExecutor, SyncExecutor, BackendMethodTask
+from nodeconductor.structure import tasks, executors
 
 
-class SecurityGroupCreateExecutor(SyncExecutor):
-
-    @classmethod
-    def get_tasks(self, security_group, **kwargs):
-        return BackendMethodTask().si(security_group, 'create_security_group', state_transition='begin_syncing')
-
-
-class SecurityGroupUpdateExecutor(SyncExecutor):
-
-    @classmethod
-    def pre_execute(self, security_group, **kwargs):
-        security_group.schedule_syncing()
-        security_group.save()
+class SecurityGroupCreateExecutor(executors.SynchronizableCreateExecutor):
 
     @classmethod
     def get_tasks(self, security_group, **kwargs):
-        return BackendMethodTask().si(security_group, 'update_security_group', state_transition='begin_syncing')
+        return tasks.BackendMethodTask().si(security_group, 'create_security_group', state_transition='begin_syncing')
 
 
-class SecurityGroupDeleteExecutor(DeleteExecutor):
-
-    @classmethod
-    def pre_execute(self, security_group, **kwargs):
-        security_group.schedule_syncing()
-        security_group.save()
+class SecurityGroupUpdateExecutor(executors.SynchronizableUpdateExecutor):
 
     @classmethod
     def get_tasks(self, security_group, **kwargs):
-        return BackendMethodTask().si(security_group, 'delete_security_group', state_transition='begin_syncing')
+        return tasks.BackendMethodTask().si(security_group, 'update_security_group', state_transition='begin_syncing')
+
+
+class SecurityGroupDeleteExecutor(executors.SynchronizableDeleteExecutor):
+
+    @classmethod
+    def get_tasks(self, security_group, **kwargs):
+        return tasks.BackendMethodTask().si(security_group, 'delete_security_group', state_transition='begin_syncing')
---
+++ nodeconductor/structure/executors.py
@@ -0,0 +1,136 @@
+from nodeconductor.core import utils as core_utils
+from nodeconductor.structure import tasks
+
+
+class BaseExecutor(object):
+    """ Base class that corresponds logical operation with backend.
+
+    Executor describes list of low-level tasks that should be executed to
+    provide high-level operation.
+
+    Executor should handle:
+     - low-level tasks execution;
+     - models state changes;
+    """
+
+    @classmethod
+    def get_tasks(cls, serialized_instance, **kwargs):
+        """ Celery Signature or Primitive that describes executor action.
+
+        Each task should be subclass of LowLevelTask class.
+        Celery Signature and Primitives:
+         - http://docs.celeryproject.org/en/latest/userguide/canvas.html
+        Examples:
+         - to execute only one task - return Signature of necessary task: `task.si(serialized_instance)`
+         - to execute several tasks - return Chain or Chord of tasks: `chain(t1.s(), t2.s())`
+        """
+        raise NotImplementedError('Executor %s should implement method `get_tasks`' % cls.__name__)
+
+    @classmethod
+    def get_link(cls, serialized_instance, **kwargs):
+        """ Celery signature of task that should be applied on successful execution. """
+        raise NotImplementedError('Executor %s should implement method `get_link`' % cls.__name__)
+
+    @classmethod
+    def get_link_error(cls, serialized_instance, **kwargs):
+        """ Celery signature of task that should be applied on failed execution. """
+        raise NotImplementedError('Executor %s should implement method `get_link_error`' % cls.__name__)
+
+    @classmethod
+    def pre_apply(cls, instance, async=True, **kwargs):
+        """ Perform synchronous actions before tasks apply """
+        pass
+
+    @classmethod
+    def post_apply(cls, instance, async=True, **kwargs):
+        """ Perform synchronous actions after tasks apply """
+        pass
+
+    @classmethod
+    def execute(cls, instance, async=True, **kwargs):
+        """ Execute high level-operation """
+        cls.pre_apply(instance, async=async, **kwargs)
+        result = cls.apply_tasks(instance, async=async, **kwargs)
+        cls.post_apply(instance, async=async, **kwargs)
+        return result
+
+    @classmethod
+    def apply_tasks(cls, instance, async=True, **kwargs):
+        """ Serialize input data and apply tasks """
+        serialized_instance = core_utils.serialize_instance(instance)
+        # TODO: Add ability to serialize kwargs here and deserialize them in task.
+
+        tasks = cls.get_tasks(serialized_instance, **kwargs)
+        link = cls.get_link(serialized_instance, **kwargs),
+        link_error = cls.get_link_error(serialized_instance, **kwargs)
+
+        if async:
+            result = tasks.apply_async(link=link, link_error=link_error)
+        else:
+            result = tasks.apply()
+            if not result.failed():
+                link.apply()
+            else:
+                link_error.apply()
+        return result
+
+
+class ErrorExecutorMixin(object):
+    """ Set object as erred on fail. """
+
+    @classmethod
+    def get_link_error(cls, serialized_instance, **kwargs):
+        return tasks.StateTransitionTask().si(serialized_instance, state_transition='set_erred')
+
+
+class DeleteExecutorMixin(object):
+    """ Delete object on success """
+
+    @classmethod
+    def get_link(cls, serialized_instance, **kwargs):
+        return tasks.DeletionTask().si(serialized_instance)
+
+
+class SynchronizableExecutorMixin(object):
+    """ Set object in sync on success """
+
+    @classmethod
+    def get_link(cls, serialized_instance, **kwargs):
+        return tasks.StateTransitionTask().si(serialized_instance, state_transition='set_in_sync')
+
+
+class SynchronizableCreateExecutor(SynchronizableExecutorMixin, ErrorExecutorMixin, BaseExecutor):
+    """ Default states transition for Synchronizable object update.
+
+     - set object in sync on success update;
+     - mark object as erred on failed update;
+    """
+    pass
+
+
+class SynchronizableUpdateExecutor(SynchronizableExecutorMixin, ErrorExecutorMixin, BaseExecutor):
+    """ Default states transition for Synchronizable object update.
+
+     - schedule syncing before update;
+     - set object in sync on success update;
+     - mark object as erred on failed update;
+    """
+
+    @classmethod
+    def pre_apply(cls, instance, async=True, **kwargs):
+        instance.schedule_syncing()
+        instance.save()
+
+
+class SynchronizableDeleteExecutor(DeleteExecutorMixin, ErrorExecutorMixin, BaseExecutor):
+    """ Default states transition for Synchronizable object deletion.
+
+     - schedule syncing before deletion;
+     - delete object on success deletion;
+     - mark object as erred on failed deletion;
+    """
+
+    @classmethod
+    def pre_apply(cls, instance, async=True, **kwargs):
+        instance.schedule_syncing()
+        instance.save()
