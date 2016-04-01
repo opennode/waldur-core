4e87549076a52afba2130a77356cffb8f7a1402a
Victor Mireyev <victor@opennodecloud.com>
2016-04-01 12:26:22 +0300

    Make permissions declaration DRY (NC-1282)

--- nodeconductor/openstack/perms.py
+++ nodeconductor/openstack/perms.py
@@ -3,24 +3,8 @@ from nodeconductor.structure import models as structure_models, perms as structu
 
 
 PERMISSION_LOGICS = (
-    ('openstack.OpenStackService', FilteredCollaboratorsPermissionLogic(
-        collaborators_query='customer__roles__permission_group__user',
-        collaborators_filter={
-            'customer__roles__role_type': structure_models.CustomerRole.OWNER,
-        },
-        any_permission=True,
-    )),
-    ('openstack.OpenStackServiceProjectLink', FilteredCollaboratorsPermissionLogic(
-        collaborators_query=[
-            'service__customer__roles__permission_group__user',
-            'project__project_groups__roles__permission_group__user',
-        ],
-        collaborators_filter=[
-            {'service__customer__roles__role_type': structure_models.CustomerRole.OWNER},
-            {'project__project_groups__roles__role_type': structure_models.ProjectGroupRole.MANAGER},
-        ],
-        any_permission=True,
-    )),
+    ('openstack.OpenStackService', structure_perms.service_permission_logic),
+    ('openstack.OpenStackServiceProjectLink', structure_perms.service_project_link_permission_logic),
     ('openstack.SecurityGroup', FilteredCollaboratorsPermissionLogic(
         collaborators_query=[
             'service_project_link__service__customer__roles__permission_group__user',
