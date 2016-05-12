018a1090b05013203797ddb8e0f2e60184a7eefa
Victor Mireyev <victor@opennodecloud.com>
2016-05-12 12:35:30 +0300

    Extract refresh token method to mixin (NC-1386)

--- nodeconductor/core/views.py
+++ nodeconductor/core/views.py
@@ -27,7 +27,25 @@ from nodeconductor.logging.loggers import event_logger
 logger = logging.getLogger(__name__)
 
 
-class ObtainAuthToken(APIView):
+class RefreshTokenMixin(object):
+    """
+    This mixin is used in both password and social auth (implemented via plugin).
+    Mixin allows to create new token if it does not exist yet or if it has already expired.
+    Token is refreshed if it has not expired yet.
+    """
+    def refresh_token(self, user):
+        token, _ = Token.objects.get_or_create(user=user)
+        lifetime = settings.NODECONDUCTOR.get('TOKEN_LIFETIME', timezone.timedelta(hours=1))
+        if token.created < timezone.now() - lifetime:
+            token.delete()
+            token = Token.objects.create(user=user)
+        else:
+            token.created = timezone.now()
+            token.save()
+        return token
+
+
+class ObtainAuthToken(RefreshTokenMixin, APIView):
     """
     Api view loosely based on DRF's default ObtainAuthToken,
     but with the responses formats and status codes aligned with BasicAuthentication behavior.
@@ -114,14 +132,7 @@ class ObtainAuthToken(APIView):
                 status=status.HTTP_401_UNAUTHORIZED,
             )
 
-        token, _ = Token.objects.get_or_create(user=user)
-        lifetime = settings.NODECONDUCTOR.get('TOKEN_LIFETIME', timezone.timedelta(hours=1))
-        if token.created < timezone.now() - lifetime:
-            token.delete()
-            token = Token.objects.create(user=user)
-        else:
-            token.created = timezone.now()
-            token.save()
+        token = self.refresh_token(user)
 
         logger.debug('Returning token for successful login of user %s', user)
         event_logger.auth.info(
