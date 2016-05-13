ac640698a8d0504005f557b2aa280b42e10e9788
Roman Kosenko <Roman.Kosenko@gmail.com>
2016-05-13 15:00:20 +0000

    Drop resource quotas test
    - NC-986

--- nodeconductor/structure/tests/test_resource.py
+++ nodeconductor/structure/tests/test_resource.py
@@ -2,44 +2,8 @@ import unittest
 
 from rest_framework import test, status
 
-from nodeconductor.structure.models import CustomerRole, Resource
+from nodeconductor.structure.models import Resource
 from nodeconductor.structure.tests import factories
-
-
-class ResourceQuotasTest(test.APITransactionTestCase):
-
-    def setUp(self):
-        self.user = factories.UserFactory()
-        self.customer = factories.CustomerFactory()
-        self.customer.add_user(self.user, CustomerRole.OWNER)
-        self.project = factories.ProjectFactory(customer=self.customer)
-
-    def test_auto_quotas_update(self):
-        settings = factories.ServiceSettingsFactory(customer=self.customer, shared=False)
-        service = factories.TestServiceFactory(customer=self.customer, settings=settings)
-
-        data = {'cores': 4, 'ram': 1024, 'disk': 20480}
-
-        service_project_link = factories.TestServiceProjectLinkFactory(service=service, project=self.project)
-        resource = factories.TestInstanceFactory(service_project_link=service_project_link, cores=data['cores'])
-
-        self.assertEqual(service_project_link.quotas.get(name='instances').usage, 1)
-        self.assertEqual(service_project_link.quotas.get(name='vcpu').usage, data['cores'])
-        self.assertEqual(service_project_link.quotas.get(name='ram').usage, 0)
-        self.assertEqual(service_project_link.quotas.get(name='storage').usage, 0)
-
-        resource.ram = data['ram']
-        resource.disk = data['disk']
-        resource.save()
-
-        self.assertEqual(service_project_link.quotas.get(name='ram').usage, data['ram'])
-        self.assertEqual(service_project_link.quotas.get(name='storage').usage, data['disk'])
-
-        resource.delete()
-        self.assertEqual(service_project_link.quotas.get(name='instances').usage, 0)
-        self.assertEqual(service_project_link.quotas.get(name='vcpu').usage, 0)
-        self.assertEqual(service_project_link.quotas.get(name='ram').usage, 0)
-        self.assertEqual(service_project_link.quotas.get(name='storage').usage, 0)
 
 
 @unittest.skip("NC-1392: Test resource's view should be available")
