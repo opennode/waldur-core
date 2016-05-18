ed088894e7a426fdfac89c8d70ae93987586a462
Juri Hudolejev <jhudolejev@gmail.com>
2016-05-18 15:40:39 +0300

    Sync changes with develop

--- setup.py
+++ setup.py
@@ -49,23 +49,6 @@ install_requires = [
 ]
 
 
-# RPM installation does not need oslo, cliff and stevedore libs -
-# they are required only for installation with setuptools
-try:
-    action = sys.argv[1]
-except IndexError:
-    pass
-else:
-    if action in ['develop', 'install', 'test', 'bdist_egg']:
-        install_requires += [
-            'cliff==1.7.0',
-            'oslo.config==1.4.0',
-            'oslo.i18n==1.0.0',
-            'oslo.utils==1.0.0',
-            'stevedore==1.0.0',
-        ]
-
-
 setup(
     name='nodeconductor',
     version='0.97.0',
