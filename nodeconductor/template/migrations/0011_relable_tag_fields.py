68820f31ff07a333ad9485c61784d41083ac3262
Ilja Livenson <ilja@opennodecloud.com>
2016-07-04 10:14:32 +0300

    Add missing migration

---
+++ nodeconductor/template/migrations/0011_relable_tag_fields.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import taggit.managers
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('template', '0010_templategroupresult_provisioned_resources_data'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='template',
+            name='tags',
+            field=taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', blank=True, help_text='A comma-separated list of tags.', verbose_name='Tags'),
+        ),
+        migrations.AlterField(
+            model_name='templategroup',
+            name='tags',
+            field=taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', blank=True, help_text='A comma-separated list of tags.', verbose_name='Tags'),
+        ),
+    ]
