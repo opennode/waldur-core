46d86dbd0392394196d5455fd4ca11bd153a3bc0
Pavel Marchuk <marchukpavelp@gmail.com>
2016-09-15 21:01:01 +0300

    Make sure that assembly is registered last.
     - nc-1558

--- nodeconductor/core/__init__.py
+++ nodeconductor/core/__init__.py
@@ -35,13 +35,24 @@ class NodeConductorExtension(object):
         """ Returns a dictionary with celery tasks which will be added to CELERYBEAT_SCHEDULE """
         return dict()
 
+    @staticmethod
+    def is_assembly():
+        """ Return True if plugin is assembly and should be installed last """
+        return False
+
     @classmethod
     def get_extensions(cls):
         """ Get a list of available extensions """
+        assemblies = []
         for nodeconductor_extension in pkg_resources.iter_entry_points('nodeconductor_extensions'):
             extension_module = nodeconductor_extension.load()
             if inspect.isclass(extension_module) and issubclass(extension_module, cls):
-                yield extension_module
+                if not extension_module.is_assembly():
+                    yield extension_module
+                else:
+                    assemblies.append(extension_module)
+        for assembly in assemblies:
+            yield assembly
 
     @classmethod
     def is_installed(cls, extension):
