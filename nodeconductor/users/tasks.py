a1c17b7ace79d77174404eede87d47a61fb7a188
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2016-10-18 10:14:27 +0300

    Expose email send on invitation creation
    - WAL-66

--- nodeconductor/users/tasks.py
+++ nodeconductor/users/tasks.py
@@ -1,8 +1,14 @@
+import logging
 
 from celery import shared_task
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.utils import timezone
 from nodeconductor.users import models
+
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task(name='nodeconductor.users.cancel_expired_invitations')
@@ -16,3 +22,32 @@ def cancel_expired_invitations(invitations=None):
         invitations = models.Invitation.objects.filter(state=models.Invitation.State.PENDING)
     invitations = invitations.filter(created__lte=expiration_date)
     invitations.update(state=models.Invitation.State.EXPIRED)
+
+
+@shared_task(name='nodeconductor.users.send_invitation')
+def send_invitation(invitation_uuid, sender_name):
+    invitation = models.Invitation.objects.get(uuid=invitation_uuid)
+
+    if invitation.project_role is not None:
+        context = {
+            'type': 'project',
+            'name': invitation.project_role.project.name.capitalize(),
+            'role': invitation.project_role.get_role_type_display
+        }
+    else:
+        context = {
+            'type': 'organization',
+            'name': invitation.customer.name.capitalize(),
+            'role': invitation.customer_role.get_role_type_display
+        }
+
+    context['sender'] = sender_name.title()
+    context['link'] = invitation.link_template.format(uuid=invitation_uuid)
+
+    subject = render_to_string('users/invitation_subject.txt', context)
+    text_message = render_to_string('users/invitation_message.txt', context)
+    html_message = render_to_string('users/invitation_message.html', context)
+
+    logger.debug('Sending invitation to {email} to join {name} {type} as {role}'.format(
+        email=invitation.email, **context))
+    send_mail(subject, text_message, settings.DEFAULT_FROM_EMAIL, [invitation.email])
--- nodeconductor/users/views.py
+++ nodeconductor/users/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 
 from nodeconductor.structure import filters as structure_filters
 from nodeconductor.structure import models as structure_models
-from nodeconductor.users import models, filters, serializers
+from nodeconductor.users import models, filters, serializers, tasks
 
 
 class InvitationViewSet(mixins.CreateModelMixin,
@@ -27,7 +27,7 @@ class InvitationViewSet(mixins.CreateModelMixin,
     filter_class = filters.InvitationFilter
     lookup_field = 'uuid'
 
-    def can_create_invitation_with(self, customer):
+    def can_manage_invitation_with(self, customer):
         user = self.request.user
 
         if user.is_staff:
@@ -44,10 +44,24 @@ class InvitationViewSet(mixins.CreateModelMixin,
             customer_role = serializer.validated_data.get('customer_role', {})
             customer = customer_role['customer']
 
-        if not self.can_create_invitation_with(customer):
+        if not self.can_manage_invitation_with(customer):
             raise PermissionDenied('You do not have permission to perform this action.')
 
-        return super(InvitationViewSet, self).perform_create(serializer)
+        invitation = serializer.save()
+        tasks.send_invitation.delay(invitation.uuid.hex, self.request.user.full_name or self.request.user.username)
+
+    @detail_route(methods=['post'])
+    def send(self, request, uuid=None):
+        invitation = self.get_object()
+
+        if not self.can_manage_invitation_with(invitation.customer):
+            raise PermissionDenied('You do not have permission to perform this action.')
+        elif invitation.state != models.Invitation.State.PENDING:
+            raise ValidationError('Only pending invitation can be resent.')
+
+        tasks.send_invitation.delay(invitation.uuid.hex, self.request.user.full_name or self.request.user.username)
+        return Response({'detail': "Invitation has been successfully sent."},
+                        status=status.HTTP_200_OK)
 
     @detail_route(methods=['post'])
     def cancel(self, request, uuid=None):
