e6444708df05c636d7bf756eaaa67efb6e32c8a1
Pavel Marchuk <marchukpavelp@gmail.com>
2016-11-24 18:40:15 +0200

    Fix cost_tracking autodicover call
     - nc-1575

--- nodeconductor/cost_tracking/__init__.py
+++ nodeconductor/cost_tracking/__init__.py
@@ -5,20 +5,12 @@ Allows to calculate price estimates for resources from your plugin.
 Check developer guide for more details.
 """
 import logging
-from decimal import Decimal
 
-from django.db.models import Prefetch
 from django.utils.module_loading import autodiscover_modules
-
-from nodeconductor.structure import ServiceBackendNotImplemented
 
 
 default_app_config = 'nodeconductor.cost_tracking.apps.CostTrackingConfig'
 logger = logging.getLogger(__name__)
-
-
-def autodiscover():
-    autodiscover_modules('cost_tracking')
 
 
 class ConsumableItem(object):
@@ -108,9 +100,14 @@ class ResourceNotRegisteredError(TypeError):
 
 class CostTrackingRegister(object):
     """ Register of all connected NC plugins """
-
-    _register = {}  # deprecated
     registered_resources = {}
+    is_autodiscovered = False
+
+    @classmethod
+    def autodiscover(cls):
+        if not cls.is_autodiscovered:
+            autodiscover_modules('cost_tracking')
+            cls.is_autodiscovered = True
 
     @classmethod
     def register_strategy(cls, strategy):
@@ -143,102 +140,3 @@ class CostTrackingRegister(object):
         """ Get all possible consumable items for given resource class """
         strategy = cls._get_strategy(resource_class)
         return strategy.get_consumable_items()
-
-    # XXX: deprecated. Should be removed.
-    @classmethod
-    def register(cls, app_label, backend):
-        cls._register[app_label] = backend
-
-    # XXX: deprecated. Should be removed.
-    @classmethod
-    def get_registered_backends(cls):
-        return cls._register.values()
-
-    # XXX: deprecated. Should be removed.
-    @classmethod
-    def get_resource_backend(cls, resource):
-        try:
-            return cls._register[resource._meta.app_label]
-        except KeyError:
-            raise ServiceBackendNotImplemented
-
-
-# XXX: This backend should be removed
-class CostTrackingBackend(object):
-    """ Cost tracking interface for NC plugin """
-
-    # A list of numerical <item type>'s like storage or users count
-    NUMERICAL = []
-
-    # Should be used as consistent name for different VM types
-    VM_SIZE_ITEM_TYPE = 'flavor'  # WTF?
-
-    @classmethod
-    def get_used_items(cls, resource):
-        """ Return list of items that are currently used by resource
-
-        Return format: [(<item type>, <item name>, <item usage>), ...]
-        <item usage> should be 1 for all uncountable items (flavors, installed OS ...)
-        """
-        raise NotImplementedError()
-
-    @classmethod
-    def get_service_price_list_items(cls, service):
-        """ Return list of price items for concrete service """
-        raise NotImplementedError()
-
-    @classmethod
-    def get_default_price_list_items(cls):
-        """ Return list of default price items for application """
-        raise NotImplementedError()
-
-    @classmethod
-    def get_monthly_cost_estimate(cls, resource):
-        """ Get resource monthly cost estimate.
-
-        By default monthly cost estimate is calculated as multiplication
-        of default price list items prices on used items time.
-        Cost estimate is calculated using price list of resource's service if possible.
-
-        Method should return decimal as result.
-        """
-        price_map = cls._get_price_map(resource)
-
-        monthly_cost = 0
-        for item_type, item_key, item_count in cls.get_used_items(resource):
-            key = (item_type, item_key)
-            value = price_map.get(key)
-            if value is None:
-                logger.error('Can not find price item with key "%s" and type "%s" for resource "%s"',
-                             item_key, item_type, resource)
-            else:
-                monthly_cost += value * Decimal(format(item_count, ".15g"))
-        return monthly_cost
-
-    @classmethod
-    def _get_price_map(cls, resource):
-        """
-        Return mapping from (item_type, key) to monthly_rate.
-        If service-specific price list item is unavailable, default
-        price list item is used instead for fetching monthly_rate.
-        """
-        from django.contrib.contenttypes.models import ContentType
-        from nodeconductor.cost_tracking.models import DefaultPriceListItem, PriceListItem
-        resource_content_type = ContentType.objects.get_for_model(resource)
-
-        price_list_items = PriceListItem.objects.filter(service=resource.service_project_link.service)
-        prefetch = Prefetch('pricelistitem_set', queryset=price_list_items, to_attr='service_item')
-
-        price_items = DefaultPriceListItem.objects\
-            .filter(resource_content_type=resource_content_type)\
-            .prefetch_related(prefetch)
-
-        price_map = {}
-        for item in price_items:
-            key = (item.item_type, item.key)
-            val = item.monthly_rate
-            if item.service_item:
-                # service_item list either contains one element or empty
-                val = item.service_item[0].monthly_rate
-            price_map[key] = Decimal(val)
-        return price_map
--- nodeconductor/server/urls.py
+++ nodeconductor/server/urls.py
@@ -11,7 +11,7 @@ from django.views.generic import TemplateView
 
 from nodeconductor.core import NodeConductorExtension
 from nodeconductor.core.routers import SortedDefaultRouter as DefaultRouter
-from nodeconductor.cost_tracking import urls as cost_tracking_urls, autodiscover as cost_tracking_autodiscover
+from nodeconductor.cost_tracking import urls as cost_tracking_urls, CostTrackingRegister
 from nodeconductor.logging import urls as logging_urls
 from nodeconductor.monitoring import urls as monitoring_urls
 from nodeconductor.quotas import urls as quotas_urls
@@ -21,7 +21,7 @@ from nodeconductor.users import urls as users_urls
 
 admin.autodiscover()
 permission.autodiscover()
-cost_tracking_autodiscover()
+CostTrackingRegister.autodiscover()
 
 router = DefaultRouter()
 cost_tracking_urls.register_in(router)
