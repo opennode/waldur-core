f17ab4a765e033ef42c043cdd1769cf760342990
tarasmatsyk <tamatsyk@gmail.com>
2016-12-20 11:44:24 +0200

    restructure test

--- nodeconductor/structure/tests/unittests/test_tasks.py
+++ nodeconductor/structure/tests/unittests/test_tasks.py
@@ -3,13 +3,13 @@ from mock import patch
 
 from nodeconductor.core import utils
 from nodeconductor.structure import tasks
-from nodeconductor.structure.tests import models, factories
+from nodeconductor.structure.tests import factories
 
 
-class TestTasks(TestCase):
+class TestDetectVMCoordinatesTasks(TestCase):
 
     @patch('requests.get')
-    def test_detect_vm_coordinates_sets_coordinates(self, mock_request_get):
+    def test_task_sets_coordinates(self, mock_request_get):
         ip_address = "127.0.0.1"
         expected_latitude = 20
         expected_longitude = 20
@@ -20,18 +20,18 @@ class TestTasks(TestCase):
         mock_request_get.return_value.json.return_value = response
         tasks.detect_vm_coordinates(utils.serialize_instance(instance))
 
-        instance_updated = models.TestInstance.objects.get(pk=instance.id)
-        self.assertEqual(instance_updated.latitude, expected_latitude)
-        self.assertEqual(instance_updated.longitude, expected_longitude)
+        instance.refresh_from_db()
+        self.assertEqual(instance.latitude, expected_latitude)
+        self.assertEqual(instance.longitude, expected_longitude)
 
     @patch('requests.get')
-    def test_detect_vm_coordinates_does_not_set_coordinates_if_response_is_not_ok(self, mock_request_get):
+    def test_task_does_not_set_coordinates_if_response_is_not_ok(self, mock_request_get):
         ip_address = "127.0.0.1"
         instance = factories.TestInstanceFactory(external_ips=ip_address)
 
         mock_request_get.return_value.ok = False
         tasks.detect_vm_coordinates(utils.serialize_instance(instance))
 
-        instance_updated = models.TestInstance.objects.get(pk=instance.id)
-        self.assertIsNone(instance_updated.latitude)
-        self.assertIsNone(instance_updated.longitude)
+        instance.refresh_from_db()
+        self.assertIsNone(instance.latitude)
+        self.assertIsNone(instance.longitude)
