ccf4fc9f9ca7fa0d9669472493241cfe92952f25
tarasmatsyk <tamatsyk@gmail.com>
2016-12-20 10:12:33 +0200

    remove responce mock negative scenario test

--- nodeconductor/structure/tests/unittests/test_tasks.py
+++ nodeconductor/structure/tests/unittests/test_tasks.py
@@ -1,11 +1,12 @@
 # encoding: utf-8
 
+from mock import patch
 from django.test import TestCase
+
 from nodeconductor.structure.tests import factories
 from nodeconductor.structure.tests import models
 from nodeconductor.core import utils as core_utils
 from nodeconductor.structure import tasks
-from mock import patch
 
 
 class TestTasks(TestCase):
@@ -31,13 +32,9 @@ class TestTasks(TestCase):
     @patch('requests.get')
     def test_detect_vm_coordinates_does_not_set_coordinates_if_response_is_not_ok(self, mock_request_get):
         ip_address = "127.0.0.1"
-        expected_latitude = 20
-        expected_longitude = 20
         instance = factories.TestInstanceFactory(external_ips=ip_address)
 
         mock_request_get.return_value.ok = False
-        response = {"ip": ip_address, "latitude": expected_latitude, "longitude": expected_longitude}
-        mock_request_get.return_value.json.return_value = response
         tasks.detect_vm_coordinates(core_utils.serialize_instance(instance))
 
         instance_updated = models.TestInstance.objects.get(pk=instance.id)
