5b067a4502156600a2153024437478bcb15136bc
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2017-02-07 14:40:56 +0200

    Add Swagger token based authorization
    - WAL-472

--- nodeconductor/server/base_settings.py
+++ nodeconductor/server/base_settings.py
@@ -242,4 +242,11 @@ SWAGGER_SETTINGS = {
     # If it is True, LOGIN_URL and LOGOUT_URL must be specified.
     'USE_SESSION_AUTH': False,
     'APIS_SORTER': 'alpha',
+    'SECURITY_DEFINITIONS': {
+        "api_key": {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+        },
+    },
 }
