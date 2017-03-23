9b493182e02f131e295d59fa667cf3c25dbb3c86
tarasmatsyk <tamatsyk@gmail.com>
2017-03-23 19:18:21 +0200

    Update links on dashboard [WAL-616]
    Remove AppIconList and add organization, project and user as links.
    Include service settings to the same list.

--- nodeconductor/server/admin/dashboard.py
+++ nodeconductor/server/admin/dashboard.py
@@ -1,9 +1,10 @@
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 from fluent_dashboard.dashboard import modules, FluentIndexDashboard, FluentAppIndexDashboard
 from fluent_dashboard.modules import AppIconList
 
-from nodeconductor.core import NodeConductorExtension
+from nodeconductor.core import NodeConductorExtension, models
+from nodeconductor.structure import models as structure_models
 from nodeconductor import __version__
 
 
@@ -48,6 +49,34 @@ class CustomIndexDashboard(FluentIndexDashboard):
 
         return result
 
+    def _get_quick_access_info(self):
+        default = []
+
+        for model in (structure_models.Project, structure_models.Customer, models.User):
+            default.append(self._get_link_to_model(model))
+
+        shared_service_setttings = self._get_link_to_model(structure_models.ServiceSettings)
+        erred_shared_service_settings = shared_service_setttings.copy()
+        shared_service_setttings['url'] = shared_service_setttings['url'] + '?shared__exact=1'
+        shared_service_setttings['title'] = _('Shared service settings')
+        default.append(shared_service_setttings)
+
+        erred_state = structure_models.ServiceSettings.States.ERRED
+        erred_shared_service_settings['url'] = shared_service_setttings['url'] + '&state__exact=' + str(erred_state)
+        settings_in_erred_state = structure_models.ServiceSettings.objects.filter(state=erred_state).count()
+        erred_settings_title = '{0} {1}'.format(settings_in_erred_state, _('shared settings in ERRED state'))
+        erred_shared_service_settings['title'] = erred_settings_title
+        default.append(erred_shared_service_settings)
+
+        return default
+
+    def _get_link_to_model(self, model):
+        return {
+            'title': str(model._meta.verbose_name_plural).capitalize(),
+            'url': reverse("admin:%s_%s_changelist" % (model._meta.app_label, model._meta.model_name)),
+            'external': True,
+        }
+
     def __init__(self, **kwargs):
         FluentIndexDashboard.__init__(self, **kwargs)
 
@@ -58,11 +87,6 @@ class CustomIndexDashboard(FluentIndexDashboard):
             billing_models.append('nodeconductor_paypal.models.Payment')
 
         self.children.append(AppIconList(_('Billing'), models=billing_models))
-        self.children.append(AppIconList(_('Structure'), models=(
-            'nodeconductor.structure.models.Customer',
-            'nodeconductor.structure.models.Project',
-            'nodeconductor.core.models.User',
-        )))
 
         self.children.append(modules.LinkList(
             _('Installed components'),
@@ -74,8 +98,9 @@ class CustomIndexDashboard(FluentIndexDashboard):
         ))
 
         self.children.append(modules.LinkList(
-            _('Quick links'),
-            children=settings.FLUENT_DASHBOARD_QUICK_LINKS))
+            _('Quick access'),
+            children=self._get_quick_access_info())
+        )
 
 
 class CustomAppIndexDashboard(FluentAppIndexDashboard):
