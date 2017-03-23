70e6f966593955521150a38bdf5323a5ae04b084
tarasmatsyk <tamatsyk@gmail.com>
2017-03-23 09:46:41 +0200

    Remove debugging urls [WAL-616]

--- nodeconductor/server/urls.py
+++ nodeconductor/server/urls.py
@@ -60,9 +60,3 @@ if settings.DEBUG:
 
     # enable login/logout for web UI in debug mode
     urlpatterns += url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
