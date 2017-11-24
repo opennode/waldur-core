9918cfa4d3821caefded6aff6b0e35969ba4fe3e
Victor Mireyev <victor@opennodecloud.com>
2017-11-24 17:45:57 +0300

    Allow to provision virtual machine using shared SSH public key [SENTRY-1504] It allows to provision virtual machine via Ansible playbook so that Waldur would be able to log in to newly provisioned virtual machine, install and configure software inside of it.

--- nodeconductor/structure/serializers.py
+++ nodeconductor/structure/serializers.py
@@ -11,6 +11,7 @@ from django.contrib import auth
 import django.core.exceptions as django_exceptions
 from django.core.validators import RegexValidator, MaxLengthValidator
 from django.db import models as django_models, transaction
+from django.db.models import Q
 from django.utils import six, timezone
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
@@ -1516,11 +1517,14 @@ class VirtualMachineSerializer(BaseResourceSerializer):
             ssh_public_key = fields.get('ssh_public_key')
             if ssh_public_key:
                 ssh_public_key.query_params = {'user_uuid': user.uuid.hex}
-                ssh_public_key.queryset = ssh_public_key.queryset.filter(user=user)
+                if not user.is_staff:
+                    subquery = Q(user=user) | Q(is_shared=True)
+                    ssh_public_key.queryset = ssh_public_key.queryset.filter(subquery)
         return fields
 
     def create(self, validated_data):
-        validated_data['image_name'] = validated_data['image'].name
+        if 'image' in validated_data:
+            validated_data['image_name'] = validated_data['image'].name
         return super(VirtualMachineSerializer, self).create(validated_data)
 
 
