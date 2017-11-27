04a52802205d731a7b627d21086e93a21e21bc14
Jenkins <jenkins@opennodecloud.com>
2017-11-27 14:28:06 +0000

    Preparing new release: 0.150.5

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ install_requires = [
 
 setup(
     name='nodeconductor',
-    version='0.150.4',
+    version='0.150.5',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/waldur-core',
