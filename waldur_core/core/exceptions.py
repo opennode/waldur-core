ce8671d2267a13371f94fb614f9398eee7e9d9a4
Victor Mireyev <victor@opennodecloud.com>
2018-03-01 12:42:45 +0300

    Move PollRuntimeStateTask and PollBackendCheckTask from plugins to core [SENTRY-2460]

--- waldur_core/core/exceptions.py
+++ waldur_core/core/exceptions.py
@@ -8,3 +8,7 @@ from rest_framework.exceptions import APIException
 class IncorrectStateException(APIException):
     status_code = status.HTTP_409_CONFLICT
     default_detail = _('Cannot modify an object in its current state.')
+
+
+class RuntimeStateException(Exception):
+    pass
