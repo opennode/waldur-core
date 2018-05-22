aa8c51cbedf62db4eac5d7ed3d386c176e291223
Jenkins <jenkins@opennodecloud.com>
2018-05-22 08:56:37 +0000

    Preparing new release: 0.160.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ install_requires = [
 
 setup(
     name='waldur_core',
-    version='0.159.2',
+    version='0.160.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://github.com/opennode/waldur-core',
